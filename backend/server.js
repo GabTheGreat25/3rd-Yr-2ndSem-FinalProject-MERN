a22be0ca784438f0cf0689bac18ef6d6c13e0255
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-03-10 22:23:12 +0800

    Update server js file

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,62 @@
-require("dotenv").config({ path: "./config/.env" });
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const corsOptions = require("./config/corsOptions");
-const mongoose = require("mongoose");
-const { logger, logEvents } = require("./middleware/logger");
-const { errorJson, errorHandler } = require("./middleware/errorJson");
-const crashHandler = require("./middleware/crashHandler");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const users = require("./routes/user");
-const notes = require("./routes/note");
-const cameras = require("./routes/camera");
+require('dotenv').config({ path: './config/.env' })
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const mongoose = require('mongoose')
+const { logger, logEvents } = require('./middleware/logger')
+const { errorJson, errorHandler } = require('./middleware/errorJson')
+const crashHandler = require('./middleware/crashHandler')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const users = require('./routes/user')
+const notes = require('./routes/note')
+const cameras = require('./routes/camera')
+const transactions = require('./routes/transactions')
 
-const connectDB = require("./config/db");
-const PORT = process.env.PORT || 4000;
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 4000
 
-app.use(logger);
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
-app.use("/", express.static(path.join(__dirname, "/public")));
-app.use("/", require("./routes/root"));
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require('./routes/root'))
 
-app.use("/api/v1", users);
-app.use("/api/v1", notes);
-app.use("/api/v1", cameras);
+app.use('/api/v1', users)
+app.use('/api/v1', notes)
+app.use('/api/v1', cameras)
+app.use('/api/v1', transactions)
 
-app.all("*", (req, res) => {
-  const filePath = req.accepts("html")
-    ? path.join(__dirname, "views", "404.html")
-    : req.accepts("json")
-    ? { message: "404 Not Found" }
-    : "404 Not Found";
+app.all('*', (req, res) => {
+  const filePath = req.accepts('html')
+    ? path.join(__dirname, 'views', '404.html')
+    : req.accepts('json')
+    ? { message: '404 Not Found' }
+    : '404 Not Found'
 
-  res.status(404).sendFile(filePath);
-});
+  res.status(404).sendFile(filePath)
+})
 
-app.use(errorJson);
-app.use(errorHandler);
-app.use(crashHandler);
-connectDB();
+app.use(errorJson)
+app.use(errorHandler)
+app.use(crashHandler)
+connectDB()
 
 app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV} mode.`)
-);
+  console.log(
+    `Server started on port ${PORT} in ${process.env.NODE_ENV} mode.`,
+  ),
+)
 
-mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'))
 
-mongoose.connection.on("error", (err) => {
-  console.log(err);
+mongoose.connection.on('error', (err) => {
+  console.log(err)
   logEvents(
     `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
-});
+    'mongoErrLog.log',
+  )
+})
