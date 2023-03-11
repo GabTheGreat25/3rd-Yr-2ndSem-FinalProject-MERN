00a0e7451ad4a8b8deba1be18752877d9e8937bf
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-11 20:16:14 +0800

    Refactor role

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,24 +8,25 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter your name"],
     maxLength: [30, "Your name cannot exceed 30 characters"],
   },
-
   email: {
     type: String,
     required: [true, "Please enter your email"],
     unique: true,
     validate: [validator.isEmail, "Please enter valid email address"],
   },
-
   password: {
     type: String,
     required: [true, "Please enter your password"],
     minlength: [6, "Your password must be longer than 6 characters"],
     select: false,
   },
-  roles: {
-    type: [String],
-    default: ["Admin"],
-  },
+  roles: [
+    {
+      type: String,
+      enum: ["Admin", "Employee", "Customer"],
+      default: "Customer",
+    },
+  ],
   active: {
     type: Boolean,
     default: true,
