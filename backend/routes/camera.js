2c1407b6bbbae55ae35abb9f13bb399306f5421b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-20 15:12:13 +0800

    Add verify token

--- backend/routes/camera.js
+++ backend/routes/camera.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const camerasController = require("../controllers/cameraController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/cameras")
