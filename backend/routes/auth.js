e30c7bea52ffa39755642df1c42b2d78c472995d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-23 10:38:06 +0800

    Refactor auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,32 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const loginLimiter = require("../middleware/loginLimiter");
+const { METHOD, PATH } = require("../constants/index");
 
-router.route("/login").post(loginLimiter, userController.login);
+const authRoutes = [
+  {
+    method: METHOD.POST,
+    path: PATH.LOGIN,
+    middleware: [loginLimiter],
+    handler: userController.login,
+  },
+  {
+    method: METHOD.GET,
+    path: PATH.REFRESH,
+    middleware: [],
+    handler: userController.refresh,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.LOGOUT,
+    middleware: [],
+    handler: userController.logout,
+  },
+];
 
-router.route("/refresh").get(userController.refresh);
-
-router.route("/logout").post(userController.logout);
+authRoutes.forEach((route) => {
+  const { method, path, middleware, handler } = route;
+  router[method](path, ...middleware, handler);
+});
 
 module.exports = router;
