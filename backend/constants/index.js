71dbbca9b594346875dc43fd27a1f3fcb629a327
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-23 10:35:55 +0800

    Add method, role, and path

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -1,5 +1,11 @@
 const RESOURCE = require("./resource");
+const METHOD = require("./method");
+const ROLE = require("./role");
+const PATH = require("./path");
 
 module.exports = {
   RESOURCE,
+  METHOD,
+  ROLE,
+  PATH,
 };
