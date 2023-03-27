aee589a4dba80be30c8cb7be695be32e1e35d792
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-27 21:13:58 +0800

    Add status code contant

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -2,10 +2,12 @@ const RESOURCE = require("./resource");
 const METHOD = require("./method");
 const ROLE = require("./role");
 const PATH = require("./path");
+const STATUSCODE = require("./statuscode");
 
 module.exports = {
   RESOURCE,
   METHOD,
   ROLE,
   PATH,
+  STATUSCODE,
 };
