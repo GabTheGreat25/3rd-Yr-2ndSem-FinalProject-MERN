c51bb5e5886789bc1b41c58a70d797bf0fedd362
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-01 13:52:11 +0800

    Set up vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vitejs.dev/config/
+const srcPath = fileURLToPath(new URL("./src", import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": srcPath,
+      // "@components": `${srcPath}/components`,
+      // "@features": `${srcPath}/features`,
+      // "@api": `${srcPath}/app/api`,
+    },
+  },
+  server: {
+    port: 6969,
+  },
+});
