e1592026f7b4f2f2202cff8a650983fc06dfc532
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-08 14:17:13 +0800

    Add dashboard home

---
+++ frontend/src/page/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DashboardLayout } from "@/component";
+
+export default function () {
+  return (
+    <>
+      <DashboardLayout>
+        <div>Home Works!</div>
+      </DashboardLayout>
+    </>
+  );
+}
