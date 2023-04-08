8e989fa9cff8bc94f1f513db375960a473cb6e0d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-08 14:15:48 +0800

    Update index component

--- frontend/src/component/index.js
+++ frontend/src/component/index.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+import DashboardLayout from "./DashboardLayout";
+import SidebarLink from "./SidebarLink";
 
-export { Navbar, Sidebar, Footer };
+export { Navbar, Sidebar, Footer, DashboardLayout, SidebarLink };
