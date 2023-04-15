20dfb46a690e8880c8a6dc8f40f16e8dfd18e997
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-15 22:28:56 +0800

    Add user CRUD api call

---
+++ frontend/src/state/api/routes/users.js
@@ -0,0 +1,53 @@
+import { ROUTE, TAGS } from "../../../constants";
+
+export const get = (builder) => {
+  return builder.query({
+    query: () => `${ROUTE.USERS_ROUTE}`,
+    method: "GET",
+    providesTags: [TAGS.USERS],
+  });
+};
+
+export const getById = (builder) => {
+  return builder.query({
+    query: (id) => `${ROUTE.USER_ID_ROUTE.replace(":id", id)}`,
+    method: "GET",
+    providesTags: [TAGS.USERS],
+  });
+};
+
+export const add = (builder) => {
+  return builder.mutation({
+    query: (payload) => ({
+      url: `${ROUTE.USERS_ROUTE}`,
+      method: "POST",
+      body: payload,
+    }),
+    invalidatesTags: [TAGS.USERS],
+  });
+};
+
+export const updateById = (builder) => {
+  return builder.mutation({
+    query: ({ id, ...payload }) => {
+      return {
+        url: `${ROUTE.USER_ID_ROUTE.replace(":id", id)}`,
+        method: "PATCH",
+        body: payload,
+      };
+    },
+    invalidatesTags: [TAGS.USERS],
+  });
+};
+
+export const deleteById = (builder) => {
+  return builder.mutation({
+    query: (id) => ({
+      url: `${ROUTE.USER_ID_ROUTE.replace(":id", id)}`,
+      method: "DELETE",
+    }),
+    invalidatesTags: [TAGS.USERS],
+  });
+};
+
+export default { get, getById, add, updateById, deleteById };
