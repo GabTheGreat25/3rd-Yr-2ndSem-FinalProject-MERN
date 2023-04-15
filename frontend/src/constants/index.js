3e36aee661184b5428bc85a9e2959f11463c51ba
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-15 22:26:45 +0800

    Add new import in constant

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,7 @@
+import TAGS from "./tags";
+import ROUTE from "./route";
+import API from "./api";
+import ERROR from "./error";
+import ROLES from "./roles";
+
+export { TAGS, ROUTE, API, ERROR, ROLES };
