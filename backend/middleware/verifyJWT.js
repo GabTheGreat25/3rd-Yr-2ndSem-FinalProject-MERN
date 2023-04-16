ed2389702e5d95057bd62d9352e86b1b2385ee8b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-16 12:03:30 +0800

    Refactor verifyJWT

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,35 +1,36 @@
-const { verifyAccessToken } = require('../utils/token')
-
-const ErrorHandler = require('../utils/errorHandler')
-const { isUserLoggedIn } = require('../services/userService')
+const { verifyAccessToken } = require("../utils/token");
+const ErrorHandler = require("../utils/errorHandler");
+const { isUserLoggedIn } = require("../services/userService");
 
 exports.verifyJWT = (req, res, next) => {
   if (!isUserLoggedIn(req.cookies))
-    throw new ErrorHandler('You are not logged in.')
+    throw new ErrorHandler("You are not logged in.");
 
-  const authHeader = req.headers.authorization || req.headers.Authorization
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
   if (!authHeader?.match(/^Bearer\s+(.*)$/))
-    throw new ErrorHandler('Please Log In First')
+    throw new ErrorHandler("Please Log In First");
 
-  const token = authHeader.match(/^Bearer\s+(.*)$/)[1]
+  const token = authHeader.match(/^Bearer\s+(.*)$/)[1];
 
-  const decoded = verifyAccessToken(token)
-  req.user = decoded.UserInfo.email
-  req.roles = decoded.UserInfo.roles
+  const decoded = verifyAccessToken(token);
+  req.user = decoded.UserInfo.email;
+  req.roles = decoded.UserInfo.roles;
 
-  next()
-}
+  next();
+};
 
-exports.authorizeRoles = (...allowedRoles) => (req, res, next) =>
-  allowedRoles.length === 0 || !req.roles
-    ? next()
-    : req.roles.some((role) => allowedRoles.includes(role))
-    ? next()
-    : next(
-        new ErrorHandler(
-          `Roles ${req.roles.join(
-            ',',
-          )} are not allowed to access this resource`,
-        ),
-      )
+exports.authorizeRoles =
+  (...allowedRoles) =>
+  (req, res, next) =>
+    allowedRoles.length === 0 || !req.roles
+      ? next()
+      : req.roles.some((role) => allowedRoles.includes(role))
+      ? next()
+      : next(
+          new ErrorHandler(
+            `Roles ${req.roles.join(
+              ","
+            )} are not allowed to access this resource`
+          )
+        );
