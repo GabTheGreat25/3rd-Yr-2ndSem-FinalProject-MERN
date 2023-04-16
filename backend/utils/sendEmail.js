0287dbf91c21224dab42a4373f1113143a87c5b7
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-16 12:04:49 +0800

    Refactor sendEmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
@@ -8,14 +8,17 @@ const sendEmail = async (options) => {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
     },
-  })
+  });
+
   const message = {
     from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
-    to: options.email,
+    to: options.to,
     subject: options.subject,
     text: options.message,
     html: options.html,
-  }
-  await transporter.sendMail(message)
-}
-module.exports = sendEmail
+  };
+
+  await transporter.sendMail(message);
+};
+
+module.exports = { sendEmail };
