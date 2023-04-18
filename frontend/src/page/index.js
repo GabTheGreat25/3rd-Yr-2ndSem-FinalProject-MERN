d216d7c219e20930134b68365ffd5422c561b332
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-18 09:47:02 +0800

    Import edit user

--- frontend/src/page/index.js
+++ frontend/src/page/index.js
@@ -4,5 +4,14 @@ import UserLogin from "./UserLogin";
 import UserRegister from "./UserRegister";
 import GetPerUser from "./User/GetPerUser";
 import CreateUser from "./User/CreateUser";
+import EditUser from "./User/EditUser.jsx";
 
-export { Home, User, UserLogin, UserRegister, GetPerUser, CreateUser };
+export {
+  Home,
+  User,
+  UserLogin,
+  UserRegister,
+  GetPerUser,
+  CreateUser,
+  EditUser,
+};
