a6a60b2b9830de9127e746466edef881f9c4b24e
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-19 01:52:00 +0800

    Add dashboard

---
+++ frontend/src/page/Dashboard/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box } from "@mui/material";
+import GetAllUser from "./getAllUser";
+import GetAllAdmin from "./GetAllAdmin";
+import GetAllEmployee from "./GetAllEmployee";
+import GetAllCustomer from "./GetAllCustomer";
+import ShowActiveUser from "./ShowActiveUser";
+
+export default function () {
+  return (
+    <>
+      <Box sx={{ mb: "1rem", mt: ".5rem" }}>
+        <GetAllUser />
+      </Box>
+      <Box sx={{ mb: "1rem", mt: ".5rem" }}>
+        <GetAllAdmin />
+      </Box>
+      <Box sx={{ mb: "1rem", mt: ".5rem" }}>
+        <GetAllEmployee />
+      </Box>
+      <Box sx={{ mb: "1rem", mt: ".5rem" }}>
+        <GetAllCustomer />
+      </Box>
+      <Box sx={{ mb: "1rem", mt: ".5rem" }}>
+        <ShowActiveUser />
+      </Box>
+    </>
+  );
+}
