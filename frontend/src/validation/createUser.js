2a3cea138904260985b068451102e9367f9b78b4
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-18 09:47:59 +0800

    Remove the commented image validation

--- frontend/src/validation/createUser.js
+++ frontend/src/validation/createUser.js
@@ -10,14 +10,4 @@ export default yup.object({
     .string("Enter your password")
     .min(8, "Password should be of minimum 8 characters length")
     .required("Password is required"),
-  // image: yup
-  //   .array()
-  //   .min(1, "Please select at least one image")
-  //   .of(
-  //     yup.mixed().test(
-  //       "fileSize",
-  //       "File size is too large",
-  //       (value) => value && value.size <= 5000000 //5mb
-  //     )
-  //   ),
 });
