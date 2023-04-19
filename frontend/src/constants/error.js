4edf3194ca1e8471dc5d345c30c144ed1aad3bb7
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-19 23:08:01 +0800

    Add note error

--- frontend/src/constants/error.js
+++ frontend/src/constants/error.js
@@ -1,4 +1,6 @@
 export default {
   GET_USERS_ERROR: "Can't receive user data.",
   DELETE_USER_ERROR: "Error deleting user.",
+  GET_NOTES_ERROR: "Can't receive note data.",
+  DELETE_NOTE_ERROR: "Error deleting note.",
 };
