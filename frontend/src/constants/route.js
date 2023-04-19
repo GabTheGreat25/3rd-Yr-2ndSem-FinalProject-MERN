2e497784b179bf81b1da38678e5afda27fa1c0c1
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-19 23:08:14 +0800

    Add note route in constant

--- frontend/src/constants/route.js
+++ frontend/src/constants/route.js
@@ -2,4 +2,7 @@ export default {
   USERS_ROUTE: "users",
   USER_ID_ROUTE: "user/:id",
   EDIT_USER_ID: "user/edit/:id",
+  NOTES_ROUTE: "notes",
+  NOTE_ID_ROUTE: "note/:id",
+  EDIT_NOTE_ID: "note/edit/:id",
 };
