523bdd3b943345e0c33605e44d06d168513f4ba2
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-19 23:11:20 +0800

    Import note validations

--- frontend/src/validation/index.js
+++ frontend/src/validation/index.js
@@ -1,4 +1,11 @@
 import createUserValidation from "./createUser";
 import editUserValidation from "./editUser";
+import createNoteValidation from "./createNote";
+import editNoteValidation from "./editNote";
 
-export { createUserValidation, editUserValidation };
+export {
+  createUserValidation,
+  editUserValidation,
+  createNoteValidation,
+  editNoteValidation,
+};
