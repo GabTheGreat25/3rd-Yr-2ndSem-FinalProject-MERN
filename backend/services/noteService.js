b0fcc40da71011d92bbd6f488246b668dcbfe23f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-19 23:07:32 +0800

    Refactor noteService

--- backend/services/noteService.js
+++ backend/services/noteService.js
@@ -1,99 +1,99 @@
-const Note = require('../models/Note')
-const ErrorHandler = require('../utils/errorHandler')
-const mongoose = require('mongoose')
+const Note = require("../models/Note");
+const ErrorHandler = require("../utils/errorHandler");
+const mongoose = require("mongoose");
 
 exports.getAllNotesData = async (page, limit, search, sort, filter) => {
-  const skip = (page - 1) * limit
+  const skip = (page - 1) * limit;
 
   let notesQuery = Note.find()
-    .populate({ path: 'user', select: 'name' })
+    .populate({ path: "user", select: "name" })
     .skip(skip)
-    .limit(limit)
+    .limit(limit);
 
   // Apply search option
   if (search) {
-    notesQuery = notesQuery.where('title').regex(new RegExp(search, 'i'))
+    notesQuery = notesQuery.where("title").regex(new RegExp(search, "i"));
   }
 
   // Apply sort option
   if (sort) {
-    const [field, order] = sort.split(':')
-    notesQuery = notesQuery.sort({ [field]: order === 'asc' ? 1 : -1 })
+    const [field, order] = sort.split(":");
+    notesQuery = notesQuery.sort({ [field]: order === "asc" ? 1 : -1 });
   } else {
-    notesQuery = notesQuery.sort({ createdAt: -1 })
+    notesQuery = notesQuery.sort({ createdAt: -1 });
   }
 
   // Apply filter option
   if (filter) {
-    const [field, value] = filter.split(':')
-    notesQuery = notesQuery.where(field).equals(value)
+    const [field, value] = filter.split(":");
+    notesQuery = notesQuery.where(field).equals(value);
   }
 
-  const notes = await notesQuery.lean().exec()
+  const notes = await notesQuery.lean().exec();
 
-  return notes
-}
+  return notes;
+};
 
 exports.getSingleNoteData = async (id) => {
   if (!mongoose.Types.ObjectId.isValid(id))
-    throw new ErrorHandler(`Invalid note ID: ${id}`)
+    throw new ErrorHandler(`Invalid note ID: ${id}`);
 
   const note = await Note.findById(id)
-    .populate({ path: 'user', select: 'name' })
+    .populate({ path: "user", select: "name" })
     .lean()
-    .exec()
+    .exec();
 
-  if (!note) throw new ErrorHandler(`Note not found with ID: ${id}`)
+  if (!note) throw new ErrorHandler(`Note not found with ID: ${id}`);
 
-  return note
-}
+  return note;
+};
 
 exports.CreateNoteData = async (req, res) => {
   const duplicateNote = await Note.findOne({ title: req.body.title })
-    .collation({ locale: 'en' })
+    .collation({ locale: "en" })
     .lean()
-    .exec()
+    .exec();
 
-  if (duplicateNote) throw new ErrorHandler('Duplicate title')
+  if (duplicateNote) throw new ErrorHandler("Duplicate title");
 
-  const note = await Note.create(req.body)
+  const note = await Note.create(req.body);
 
-  return note
-}
+  return note;
+};
 
 exports.updateNoteData = async (req, res, id) => {
   if (!mongoose.Types.ObjectId.isValid(id))
-    throw new ErrorHandler(`Invalid note ID: ${id}`)
+    throw new ErrorHandler(`Invalid note ID: ${id}`);
 
   const updatedNote = await Note.findByIdAndUpdate(id, req.body, {
     new: true,
     runValidators: true,
   })
     .lean()
-    .exec()
+    .exec();
 
-  if (!updatedNote) throw new ErrorHandler(`Note not found with ID: ${id}`)
+  if (!updatedNote) throw new ErrorHandler(`Note not found with ID: ${id}`);
 
   const duplicate = await Note.findOne({
     title: req.body.title,
     _id: { $ne: id },
   })
-    .collation({ locale: 'en' })
+    .collation({ locale: "en" })
     .lean()
-    .exec()
+    .exec();
 
-  if (duplicate) throw new ErrorHandler('Duplicate title')
+  if (duplicate) throw new ErrorHandler("Duplicate title");
 
-  return updatedNote
-}
+  return updatedNote;
+};
 
 exports.deleteNoteData = async (id) => {
   if (!mongoose.Types.ObjectId.isValid(id))
-    throw new ErrorHandler(`Invalid note ID: ${id}`)
+    throw new ErrorHandler(`Invalid note ID: ${id}`);
 
-  if (!id) throw new ErrorHandler(`Note not found with ID: ${id}`)
+  if (!id) throw new ErrorHandler(`Note not found with ID: ${id}`);
 
-  const note = await Note.findOneAndDelete({ _id: id }).lean().exec()
+  const note = await Note.findOneAndDelete({ _id: id }).lean().exec();
 
-  return note
-}
+  return note;
+};
