9f9ac2b2adc197f518c4951679fd5c9e0ae3d549
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-04-21 17:14:50 +0800

    Add pagination in DataTable

--- frontend/src/component/DataTable.jsx
+++ frontend/src/component/DataTable.jsx
@@ -1,40 +1,50 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import { generateKey } from "../services/generateKey";
-import { splitKey, deconstruct, manipulate } from "../services/dataTable";
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Pagination from '@mui/material/Pagination'
+import { generateKey } from '../services/generateKey'
+import { splitKey, deconstruct, manipulate } from '../services/dataTable'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#2c3e50",
-    color: "#f1f2f6",
+    backgroundColor: '#2c3e50',
+    color: '#f1f2f6',
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
-}));
+}))
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
+  '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  "&:last-child td, &:last-child th": {
+  '&:last-child td, &:last-child th': {
     border: 0,
   },
-}));
+}))
 
 export default function (props) {
-  const { headers = [], data = [], keys = [], actions = [] } = props;
+  const { headers = [], data = [], keys = [], actions = [] } = props
+  const [page, setPage] = React.useState(1)
+  const rowsPerPage = 2
 
-  const hasActions = actions.length > 0;
+  const hasActions = actions.length > 0
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const startIndex = (page - 1) * rowsPerPage
+  const endIndex = startIndex + rowsPerPage
+  const paginatedData = data.slice(startIndex, endIndex)
 
   return (
     <TableContainer component={Paper}>
@@ -53,17 +63,17 @@ export default function (props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => {
+          {paginatedData.map((row) => {
             return (
               <StyledTableRow key={generateKey(5)}>
                 {keys.map((e) => {
-                  const { key, operation } = e;
-                  const splitted = splitKey(key);
-                  const hasOperation = operation;
-                  let tempValue = row[key];
+                  const { key, operation } = e
+                  const splitted = splitKey(key)
+                  const hasOperation = operation
+                  let tempValue = row[key]
 
                   if (splitted.length > 1)
-                    tempValue = deconstruct(splitted, row);
+                    tempValue = deconstruct(splitted, row)
 
                   return (
                     <StyledTableCell key={generateKey(5)} align="center">
@@ -71,28 +81,28 @@ export default function (props) {
                         ? manipulate(tempValue, row, hasOperation)
                         : tempValue}
                     </StyledTableCell>
-                  );
+                  )
                 })}
-                {hasActions > 0 && (
+                {hasActions && (
                   <StyledTableCell align="center">
                     <ButtonGroup>
                       {actions.map((action) => (
                         <Button
                           sx={{
-                            backgroundColor: "#2c3e50",
-                            marginRight: " .5rem",
-                            color: "#dfe4ea",
-                            "&:hover": {
-                              backgroundColor: "#dfe4ea",
-                              color: "#2c3e50",
-                              transition: "transform 0.2s ease-in-out",
-                              transform: "scale(1.1)",
-                              borderColor: "#2c3e50",
+                            backgroundColor: '#2c3e50',
+                            marginRight: ' .5rem',
+                            color: '#dfe4ea',
+                            '&:hover': {
+                              backgroundColor: '#dfe4ea',
+                              color: '#2c3e50',
+                              transition: 'transform 0.2s ease-in-out',
+                              transform: 'scale(1.1)',
+                              borderColor: '#2c3e50',
                             },
                           }}
                           key={generateKey(5)}
                           onClick={() => {
-                            action.onClick(row["_id"]);
+                            action.onClick(row['_id'])
                           }}
                         >
                           {action.title}
@@ -102,10 +112,20 @@ export default function (props) {
                   </StyledTableCell>
                 )}
               </StyledTableRow>
-            );
+            )
           })}
         </TableBody>
       </Table>
+      <Pagination
+        count={Math.ceil(data.length / rowsPerPage)}
+        page={page}
+        onChange={handleChangePage}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '1rem',
+        }}
+      />
     </TableContainer>
-  );
+  )
 }
