e401809051bc195f5aa14b5e7d7bc0831330d10c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-21 13:47:41 +0800

    Import AllUserCamera

--- frontend/src/page/index.js
+++ frontend/src/page/index.js
@@ -1,24 +1,25 @@
-import Home from './Home'
-import User from './User'
-import UserLogin from './UserLogin'
-import UserRegister from './UserRegister'
-import GetPerUser from './User/GetPerUser'
-import CreateUser from './User/CreateUser'
-import EditUser from './User/EditUser.jsx'
-import Dashboard from './Dashboard'
-import GetAllUser from './Dashboard/GetAllUser'
-import GetAllAdmin from './Dashboard/GetAllAdmin'
-import GetAllEmployee from './Dashboard/GetAllEmployee'
-import GetAllCustomer from './Dashboard/GetAllCustomer'
-import ShowActiveUser from './Dashboard/ShowActiveUser'
-import Note from './Notes'
-import GetPerNote from './Notes/GetPerNote'
-import CreateNote from './Notes/CreateNote'
-import EditNote from './Notes/EditNote'
-import Camera from './Cameras'
-import GetPerCamera from './Cameras/GetPerCameras'
-import CreateCamera from './Cameras/CreateCameras'
-import EditCamera from './Cameras/EditCameras'
+import Home from "./Home";
+import User from "./User";
+import UserLogin from "./UserLogin";
+import UserRegister from "./UserRegister";
+import GetPerUser from "./User/GetPerUser";
+import CreateUser from "./User/CreateUser";
+import EditUser from "./User/EditUser.jsx";
+import Dashboard from "./Dashboard";
+import GetAllUser from "./Dashboard/GetAllUser";
+import GetAllAdmin from "./Dashboard/GetAllAdmin";
+import GetAllEmployee from "./Dashboard/GetAllEmployee";
+import GetAllCustomer from "./Dashboard/GetAllCustomer";
+import ShowActiveUser from "./Dashboard/ShowActiveUser";
+import Note from "./Notes";
+import GetPerNote from "./Notes/GetPerNote";
+import CreateNote from "./Notes/CreateNote";
+import EditNote from "./Notes/EditNote";
+import Camera from "./Cameras";
+import GetPerCamera from "./Cameras/GetPerCameras";
+import CreateCamera from "./Cameras/CreateCameras";
+import EditCamera from "./Cameras/EditCameras";
+import AllUserCamera from "./Dashboard/AllUserCamera";
 
 export {
   Home,
@@ -42,4 +43,5 @@ export {
   GetPerCamera,
   CreateCamera,
   EditCamera,
-}
+  AllUserCamera,
+};
