8957257a2d2f50dda5fa3ddc28626718c1637587
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-21 11:20:45 +0800

    Add price in validation

--- frontend/src/validation/editCamera.js
+++ frontend/src/validation/editCamera.js
@@ -1,7 +1,11 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export default yup.object({
-  name: yup.string('Enter your Name').required('Name is required'),
-  text: yup.string('Enter your Text').required('Text is required'),
-  price: yup.string('Enter your Price').required('Price is required'),
-})
+  name: yup.string().required("Name is required"),
+  text: yup.string().required("Text is required"),
+  price: yup
+    .number()
+    .required("Price is required")
+    .min(0, "Price must be at least 0")
+    .max(10000, "Price must be at most 10000"),
+});
