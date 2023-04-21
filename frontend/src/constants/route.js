86bad2320efb431e33ae18bd7a1c52f3cfcf8258
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-21 13:47:23 +0800

    Add login route

--- frontend/src/constants/route.js
+++ frontend/src/constants/route.js
@@ -1,11 +1,12 @@
 export default {
-  USERS_ROUTE: 'users',
-  USER_ID_ROUTE: 'user/:id',
-  EDIT_USER_ID: 'user/edit/:id',
-  NOTES_ROUTE: 'notes',
-  NOTE_ID_ROUTE: 'note/:id',
-  EDIT_NOTE_ID: 'note/edit/:id',
-  CAMERAS_ROUTE: 'cameras',
-  CAMERA_ID_ROUTE: 'camera/:id',
-  EDIT_CAMERA_ID: 'camera/edit/:id',
-}
+  LOGIN_ROUTE: "/login",
+  USERS_ROUTE: "users",
+  USER_ID_ROUTE: "user/:id",
+  EDIT_USER_ID: "user/edit/:id",
+  NOTES_ROUTE: "notes",
+  NOTE_ID_ROUTE: "note/:id",
+  EDIT_NOTE_ID: "note/edit/:id",
+  CAMERAS_ROUTE: "cameras",
+  CAMERA_ID_ROUTE: "camera/:id",
+  EDIT_CAMERA_ID: "camera/edit/:id",
+};
