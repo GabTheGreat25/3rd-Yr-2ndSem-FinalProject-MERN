ef948f420fa9abf5fdf0613043097eeee1938c6d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-23 22:48:05 +0800

    Add resetPassword

---
+++ frontend/src/page/ResetPassword/index.jsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { useResetPasswordMutation } from "../../state/api/reducer";
+import { Box } from "@mui/system";
+import { useFormik } from "formik";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+export default function ResetPassword() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [resetPassword] = useResetPasswordMutation();
+
+  const formik = useFormik({
+    initialValues: {
+      newPassword: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values) => {
+      const { newPassword, confirmPassword } = values;
+      resetPassword({ newPassword, confirmPassword }).then((response) => {
+        const url = `https://mailtrap.io/inboxes/1656145/messages`;
+        window.open(url, "_blank");
+        console.log("Response from API:", response);
+      });
+    },
+  });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          mt: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Reset Password
+        </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            label="New Password"
+            type={showPassword ? "text" : "password"}
+            name="newPassword"
+            value={formik.values.newPassword}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.newPassword && Boolean(formik.errors.newPassword)
+            }
+            helperText={formik.touched.newPassword && formik.errors.newPassword}
+            margin="normal"
+            required
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowPassword}>
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            label="Confirm Password"
+            type={showConfirmPassword ? "text" : "password"}
+            name="confirmPassword"
+            value={formik.values.confirmPassword}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.confirmPassword &&
+              Boolean(formik.errors.confirmPassword)
+            }
+            helperText={
+              formik.touched.confirmPassword && formik.errors.confirmPassword
+            }
+            margin="normal"
+            required
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowConfirmPassword}>
+                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Reset Password
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+}
