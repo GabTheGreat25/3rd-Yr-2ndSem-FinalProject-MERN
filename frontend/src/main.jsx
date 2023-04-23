de0fbbcac4f7c6a31b1b6a5475b56382896bc93c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-24 00:11:40 +0800

    Add ToastContainer in main jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>
