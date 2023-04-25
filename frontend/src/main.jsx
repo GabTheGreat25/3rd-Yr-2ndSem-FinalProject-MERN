b22ab062fa715e1b159188bde753736f89bb2f68
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-25 12:36:20 +0800

    Add persist in main jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./state/store";
+import { store, persistor } from "./state/store";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer />
-      <App />
+      <PersistGate persistor={persistor}>
+        <ToastContainer />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
