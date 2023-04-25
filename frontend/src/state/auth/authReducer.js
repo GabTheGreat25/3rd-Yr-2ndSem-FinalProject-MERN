143d665722eca4134f3cb306af7ab3d369fb16e6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-25 12:38:16 +0800

    Add authReducer

---
+++ frontend/src/state/auth/authReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../api/reducer";
+import { initialState } from "./state";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout(state) {
+      state.token = "";
+      state.authenticated = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      api.endpoints.login.matchFulfilled,
+      (state, { payload }) => {
+        if (payload?.success === true) {
+          state.token = payload?.details?.accessToken;
+          state.authenticated = true;
+        }
+      }
+    );
+  },
+});
+
+export const { setToken } = authSlice.actions;
+export default authSlice.reducer;
