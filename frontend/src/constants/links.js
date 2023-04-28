8d84a044087c4e11d4219308b8999a76dcabbdf3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-28 09:41:17 +0800

    Add links in constant

---
+++ frontend/src/constants/links.js
@@ -0,0 +1,43 @@
+import ICONS from "./icons";
+
+export default {
+  ADMIN: [
+    {
+      title: "Home",
+      link: "/dashboard",
+      icon: ICONS.SYMBOL.HOME,
+    },
+    {
+      title: "User",
+      link: "/dashboard/user",
+      icon: ICONS.SYMBOL.PEOPLE_ALT_SHARP,
+    },
+    {
+      title: "Note",
+      link: "/dashboard/note",
+      icon: ICONS.SYMBOL.EVENT_NOTE,
+    },
+    {
+      title: "Camera",
+      link: "/dashboard/camera",
+      icon: ICONS.SYMBOL.CAMERA,
+    },
+  ],
+  EMPLOYEE: [
+    {
+      title: "Home",
+      link: "/dashboard",
+      icon: ICONS.SYMBOL.HOME,
+    },
+    {
+      title: "Note",
+      link: "/dashboard/note",
+      icon: ICONS.SYMBOL.EVENT_NOTE,
+    },
+    {
+      title: "Camera",
+      link: "/dashboard/camera",
+      icon: ICONS.SYMBOL.CAMERA,
+    },
+  ],
+};
