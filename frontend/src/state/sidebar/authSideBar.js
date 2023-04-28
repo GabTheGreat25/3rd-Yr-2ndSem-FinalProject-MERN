6c5683a4826e3d0846f360e8762e0c6bafbf92dd
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-28 09:42:57 +0800

    Add authSideBar

---
+++ frontend/src/state/sidebar/authSideBar.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { TAGS } from "../../constants";
+import { initialState } from "./state";
+
+export const sideBarSlice = createSlice({
+  name: TAGS.AUTH,
+  initialState,
+  reducers: {
+    changeLinks(state, action) {
+      state.links = action.payload.links;
+      console.log(action.payload.links);
+    },
+  },
+});
+
+export const { changeLinks } = sideBarSlice.actions;
+
+export default sideBarSlice.reducer;
