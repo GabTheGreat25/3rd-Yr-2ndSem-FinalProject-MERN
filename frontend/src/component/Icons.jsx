c4656e45f7bf03d210086ac7aa52cdac8da61e90
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-30 00:14:15 +0800

    Add icon comment

--- frontend/src/component/Icons.jsx
+++ frontend/src/component/Icons.jsx
@@ -2,6 +2,7 @@ import Home from "@mui/icons-material/Home";
 import PeopleAltSharp from "@mui/icons-material/PeopleAltSharp";
 import EventNote from "@mui/icons-material/EventNote";
 import Camera from "@mui/icons-material/Camera";
+import Comment from "@mui/icons-material/Comment";
 import { ICONS } from "@/constants";
 
 export default function (props) {
@@ -11,6 +12,7 @@ export default function (props) {
   if (ICONS.SYMBOL.PEOPLE_ALT_SHARP === icon) return <PeopleAltSharp />;
   if (ICONS.SYMBOL.EVENT_NOTE === icon) return <EventNote />;
   if (ICONS.SYMBOL.CAMERA === icon) return <Camera />;
+  if (ICONS.SYMBOL.COMMENT === icon) return <Comment />;
 
   return <DashboardIcon />;
 }
