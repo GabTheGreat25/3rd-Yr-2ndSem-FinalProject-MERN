ce13a4990510c4c4ef65ca8e6b05ff3464201ff6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-30 00:16:10 +0800

    Refactor UnprotectedRoute

--- frontend/src/component/UnprotectedRoute.jsx
+++ frontend/src/component/UnprotectedRoute.jsx
@@ -4,16 +4,9 @@ import { Navigate } from "react-router-dom";
 export default function ({ children, unprotected = false }) {
   const auth = useSelector((state) => state.auth);
 
-  const isAdmin = auth.user?.roles?.includes("Admin");
-  const isEmployee = auth.user?.roles?.includes("Employee");
-
-  const isAuth = auth.authenticated && (isAdmin || isEmployee);
-
-  const path = isAuth ? "/dashboard" : "/customer";
-
   return unprotected || !auth.authenticated ? (
     children
   ) : (
-    <Navigate to={path} replace />
+    <Navigate to="/dashboard" replace />
   );
 }
