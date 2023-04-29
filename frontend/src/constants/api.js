2c79e30833475f12362705c0b56e483625c9031e
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-30 00:16:23 +0800

    Add transaction and comments in api

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,10 +1,16 @@
-import TAGS from './tags'
+import TAGS from "./tags";
 
 export default {
-  GET: 'GET',
-  POST: 'POST',
-  PATCH: 'PATCH',
-  PUT: 'PUT',
-  DELETE: 'DELETE',
-  TAGS: [TAGS.USERS, TAGS.NOTES, TAGS.CAMERAS],
-}
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+  TAGS: [
+    TAGS.USERS,
+    TAGS.NOTES,
+    TAGS.CAMERAS,
+    TAGS.TRANSACTIONS,
+    TAGS.COMMENTS,
+  ],
+};
