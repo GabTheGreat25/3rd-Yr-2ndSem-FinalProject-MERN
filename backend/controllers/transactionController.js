9afa6cb89d4568a2b6a8238ecb3e79724539d02f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-29 15:59:12 +0800

    Change details to transactions

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,62 +1,61 @@
-const SuccessHandler = require('../utils/successHandler')
-const ErrorHandler = require('../utils/errorHandler')
-const transactionsService = require('../services/transactionService')
-const asyncHandler = require('express-async-handler')
-const checkRequiredFields = require('../helpers/checkRequiredFields')
+const SuccessHandler = require("../utils/successHandler");
+const ErrorHandler = require("../utils/errorHandler");
+const transactionsService = require("../services/transactionService");
+const asyncHandler = require("express-async-handler");
+const checkRequiredFields = require("../helpers/checkRequiredFields");
 
 exports.getAllTransactions = asyncHandler(async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1
-  const limit = parseInt(req.query.limit) || 10
-  const search = req.query.search
-  const sort = req.query.sort
-  const filter = req.query.filter
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const search = req.query.search;
+  const sort = req.query.sort;
+  const filter = req.query.filter;
 
   const transactionsQuery = transactionsService.getAllTransactionsData(
     page,
     limit,
     search,
     sort,
-    filter,
-  )
-  const transactions = await transactionsQuery.lean()
+    filter
+  );
+  const transactions = await transactionsQuery.lean();
 
   if (!transactions.length) {
-    return next(new ErrorHandler('No transactions found'))
+    return next(new ErrorHandler("No transactions found"));
   }
 
-  const transactionStatuses = transactions.map((u) => u.status).join(', ')
-  const transactionIds = transactions.map((u) => u._id).join(', ')
+  const transactionStatuses = transactions.map((u) => u.status).join(", ");
+  const transactionIds = transactions.map((u) => u._id).join(", ");
 
   return SuccessHandler(
     res,
     `Transactions with status ${transactionStatuses} and IDs ${transactionIds} retrieved`,
     transactions.map((transaction) => ({
-      transactionId: transaction._id,
-      cameraId: transaction.cameras[0]._id,
-    })),
-  )
-})
+      transactions,
+    }))
+  );
+});
 
 exports.getSingleTransaction = asyncHandler(async (req, res, next) => {
   const transaction = await transactionsService.getSingleTransactionData(
-    req.params.id,
-  )
+    req.params.id
+  );
 
   return !transaction
-    ? next(new ErrorHandler('No transaction found'))
+    ? next(new ErrorHandler("No transaction found"))
     : SuccessHandler(
         res,
         `Transaction with ID ${transaction.id} is ${transaction.status}`,
-        transaction,
-      )
-})
+        transaction
+      );
+});
 exports.createNewTransaction = [
   asyncHandler(async (req, res, next) => {
-    const { user, status, date } = req.body
-    const cameras = req.body.cameras || []
+    const { user, status, date } = req.body;
+    const cameras = req.body.cameras || [];
 
     if (!user || !status || !date) {
-      return ErrorHandler(res, 400, 'Missing required fields')
+      return ErrorHandler(res, 400, "Missing required fields");
     }
 
     const transactionData = {
@@ -64,11 +63,11 @@ exports.createNewTransaction = [
       cameras,
       status,
       date,
-    }
+    };
 
     const transaction = await transactionsService.CreateTransactionData(
-      transactionData,
-    )
+      transactionData
+    );
 
     return SuccessHandler(res, transaction.message, {
       user: transaction.transaction.user,
@@ -77,37 +76,37 @@ exports.createNewTransaction = [
       _id: transaction.transaction._id,
       __v: transaction.transaction.__v,
       cameras: transaction.transaction.cameras,
-    })
+    });
   }),
-]
+];
 
 exports.updateTransaction = [
-  checkRequiredFields(['user', 'camera', 'status', 'date']),
+  checkRequiredFields(["user", "camera", "status", "date"]),
   asyncHandler(async (req, res, next) => {
     const transaction = await transactionsService.updateTransactionData(
       req,
       res,
-      req.params.id,
-    )
+      req.params.id
+    );
 
     return SuccessHandler(
       res,
       `Transaction on ${transaction.date} with ID ${transaction._id} is updated`,
-      transaction,
-    )
+      transaction
+    );
   }),
-]
+];
 
 exports.deleteTransaction = asyncHandler(async (req, res, next) => {
   const transaction = await transactionsService.deleteTransactionData(
-    req.params.id,
-  )
+    req.params.id
+  );
 
   return !transaction
-    ? next(new ErrorHandler('No transaction found'))
+    ? next(new ErrorHandler("No transaction found"))
     : SuccessHandler(
         res,
         `Transaction on ${transaction.date} with ID ${transaction._id} is deleted`,
-        transaction,
-      )
-})
+        transaction
+      );
+});
