7d81a19ae80bdd8e527e1cfbf920fb24e53ccf03
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-04-29 15:19:22 +0800

    Add multiple transaction in mdoels

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,28 +1,30 @@
-const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const mongoose = require('mongoose')
+const { RESOURCE } = require('../constants/index')
 
 const transactionSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: "User",
+    ref: 'User',
   },
-  camera: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "Camera",
-  },
+  cameras: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Camera',
+    },
+  ],
   status: {
     type: String,
-    default: "pending",
+    default: 'pending',
     enum: {
-      values: ["pending", "paid", "not paid", "cancelled"],
+      values: ['pending', 'paid', 'not paid', 'cancelled'],
     },
   },
   date: {
     type: Date,
-    required: [true, "Please enter a date"],
+    required: [true, 'Please enter a date'],
   },
-});
+})
 
-module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema);
+module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema)
