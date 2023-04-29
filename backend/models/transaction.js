f5cfc4850034afb1ad1cc4ea7a0bae3b4ca8fea3
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-04-29 10:43:40 +0800

    Update transaction model fields

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,28 +1,37 @@
-const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const mongoose = require('mongoose')
+const { RESOURCE } = require('../constants/index')
 
 const transactionSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: "User",
+    ref: 'User',
   },
-  camera: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "Camera",
-  },
+  cameras: [
+    {
+      camera: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Camera',
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+    },
+  ],
   status: {
     type: String,
-    default: "pending",
+    default: 'pending',
     enum: {
-      values: ["pending", "paid", "not paid", "cancelled"],
+      values: ['pending', 'paid', 'not paid', 'cancelled'],
     },
   },
   date: {
     type: Date,
-    required: [true, "Please enter a date"],
+    required: [true, 'Please enter a date'],
   },
-});
+})
 
-module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema);
+module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema)
