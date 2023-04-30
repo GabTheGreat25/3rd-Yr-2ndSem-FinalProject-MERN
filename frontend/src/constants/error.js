a8958b86e2f8392e4ee09a522de6f82777ea7b42
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-30 15:42:29 +0800

    Add transaction error

--- frontend/src/constants/error.js
+++ frontend/src/constants/error.js
@@ -7,6 +7,8 @@ export default {
   DELETE_CAMERA_ERROR: "Error deleting camera.",
   GET_COMMENTS_ERROR: "Can't receive comment data.",
   DELETE_COMMENT_ERROR: "Error deleting comment.",
+  GET_TRANSACTIONS_ERROR: "Can't receive transaction data.",
+  DELETE_TRANSACTION_ERROR: "Error deleting transaction.",
   USER_DETAILS_ERROR: "Error updating user details",
   USERS_PASSWORD_ERROR: "Error updating user password",
 };
