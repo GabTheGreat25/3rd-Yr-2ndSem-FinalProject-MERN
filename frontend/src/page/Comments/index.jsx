572e1ed16895e241f4e01579cced112b8e4bd832
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-30 15:43:00 +0800

    Refactor index comment to accept only log in users

--- frontend/src/page/Comments/index.jsx
+++ frontend/src/page/Comments/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DataTable, Button } from "@/component";
+import { DataTable } from "@/component";
 import {
   useGetTransactionsQuery,
   useGetCommentsQuery,
@@ -8,37 +8,57 @@ import { PacmanLoader } from "react-spinners";
 import { ERROR } from "../../constants";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function () {
   const navigate = useNavigate();
   const auth = useSelector((state) => state.auth);
 
   const {
-    data: dataTransaction,
+    data: transactionsData,
     isLoading: transactionIsLoading,
     isError: transactionIsError,
-  } = useGetTransactionsQuery({
-    populate: "transaction",
-  });
-
-  const userArray = dataTransaction?.details?.flatMap((detail) =>
-    detail?.transactions?.map((transaction) => transaction.user)
-  );
-
-  const filteredData = userArray?.filter(
-    (user) => user?._id === auth.user?._id
-  );
-
-  console.log(filteredData);
+  } = useGetTransactionsQuery();
 
   const { data, isLoading, isError } = useGetCommentsQuery({
     populate: "transaction",
   });
 
-  console.log(data?.details);
+  const filteredTransactions = transactionsData?.details?.filter(
+    (detail) => detail.user?._id === auth.user?._id
+  );
 
-  const headers = [];
-  const keys = [];
+  const filteredComments = data?.details?.filter((comment) =>
+    filteredTransactions?.some(
+      (transaction) => transaction?._id === comment?.transaction?._id
+    )
+  );
+
+  const headers = ["ID", "TransService", "Text", "Ratings", "Transaction"];
+  const keys = [
+    {
+      key: "_id",
+      operation: (value, row) => (
+        <Link to={`/dashboard/comment/${row._id}`} className="link">
+          {row._id}
+        </Link>
+      ),
+    },
+    {
+      key: "transService",
+    },
+    {
+      key: "text",
+    },
+    {
+      key: "ratings",
+      operation: (value, row) => `${value} stars`,
+    },
+    {
+      key: "transaction",
+      operation: (value) => (value ? value.status : ""),
+    },
+  ];
 
   const handleEdit = (id) => {
     navigate(`edit/${id}`);
@@ -53,19 +73,21 @@ export default function () {
 
   return (
     <>
-      {isLoading ? (
+      {isLoading || transactionIsLoading ? (
         <div className="loader">
           <PacmanLoader color="#2c3e50" loading={true} size={50} />
         </div>
       ) : isError ? (
         <div className="errorMessage">{ERROR.GET_COMMENTS_ERROR}</div>
+      ) : transactionIsError ? (
+        <div className="errorMessage">{ERROR.GET_TRANSACTIONS_ERROR}</div>
       ) : (
-        data && (
+        filteredComments && (
           <DataTable
             headers={headers}
             keys={keys}
             actions={actions}
-            data={data?.details}
+            data={filteredComments}
           />
         )
       )}
