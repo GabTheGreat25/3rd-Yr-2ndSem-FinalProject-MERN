d0c3ba11fc740c7e484b4784c78adcf47fb9d5e7
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-01 20:12:28 +0800

    Add CartPreview

---
+++ frontend/src/page/Transactions/CartPreview.jsx
@@ -0,0 +1,20 @@
+export default function ({ cartItems, onRemoveFromCart }) {
+  console.log(cartItems);
+  return (
+    <div className="cartPreview">
+      <h3>Cart Preview</h3>
+      {cartItems.map((item) => (
+        <div key={item._id} className="cartItem">
+          <span>{item.name}</span>
+          <button
+            onClick={() => {
+              onRemoveFromCart(item);
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
