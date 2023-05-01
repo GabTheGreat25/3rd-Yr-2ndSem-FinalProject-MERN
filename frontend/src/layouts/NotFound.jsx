369230feed2dcbfafdf276bc69a61b0213d3ba65
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-05-01 08:21:40 +0800

    Update Notfound user interface

--- frontend/src/layouts/NotFound.jsx
+++ frontend/src/layouts/NotFound.jsx
@@ -1,5 +1,24 @@
-import React from "react";
-
-export default function () {
-  return <div>Not Found Works!</div>;
+import React from 'react'
+import MyNavbar from '../component/MyNavbar'
+import Broken from '../assets/broken-camera.jpg'
+export default function NotFound() {
+  return (
+    <div>
+      <MyNavbar />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '90vh',
+        }}
+      >
+        <div style={{ textAlign: 'center' }}>
+          <img src={Broken} alt="Broken Image" style={{ width: '500px' }} />
+          <h1>Not Found</h1>
+          <p>The requested page could not be found.</p>
+        </div>
+      </div>
+    </div>
+  )
 }
