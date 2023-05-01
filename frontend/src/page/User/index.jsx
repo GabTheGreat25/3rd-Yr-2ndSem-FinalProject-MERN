4e678954ef8b384d25ecca9225f13d2f319106bb
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-01 11:40:24 +0800

    Remove onSuccess in delete mutation

--- frontend/src/page/User/index.jsx
+++ frontend/src/page/User/index.jsx
@@ -1,110 +1,104 @@
-import React from 'react'
-import { useState } from 'react'
-import { DataTable, Button } from '@/component'
-import { useGetUsersQuery, useDeleteUserMutation } from '@/state/api/reducer'
-import { PacmanLoader } from 'react-spinners'
-import { ERROR } from '../../constants'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useState } from "react";
+import { DataTable, Button } from "@/component";
+import { useGetUsersQuery, useDeleteUserMutation } from "@/state/api/reducer";
+import { PacmanLoader } from "react-spinners";
+import { ERROR } from "../../constants";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 
 export default function () {
-  const navigate = useNavigate()
-  const { data, isLoading, isError } = useGetUsersQuery()
-  const [isDeletingId, setIsDeletingId] = useState(null)
+  const navigate = useNavigate();
+  const { data, isLoading, isError } = useGetUsersQuery();
+  const [isDeletingId, setIsDeletingId] = useState(null);
 
-  const [
-    deleteUser,
-    { isLoading: isDeleting, isError: isDeleteError },
-  ] = useDeleteUserMutation({
-    onSuccess: () => {
-      queryCache.invalidateQueries('getUsers')
-    },
-  })
+  const [deleteUser, { isLoading: isDeleting, isError: isDeleteError }] =
+    useDeleteUserMutation();
 
-  const auth = useSelector((state) => state.auth)
+  const auth = useSelector((state) => state.auth);
 
-  const headers = ['ID', 'User Name', 'Email', 'Roles', 'Images']
+  const headers = ["ID", "User Name", "Email", "Roles", "Images"];
   const keys = [
     {
-      key: '_id',
+      key: "_id",
       operation: (value, row) => (
-        <Link to={`/dashboard/user/${row._id}`} className="link">
-          {row._id}
+        <Link to={`/dashboard/user/${row?._id}`} className="link">
+          {row?._id}
         </Link>
       ),
     },
     {
-      key: 'name',
+      key: "name",
     },
     {
-      key: 'email',
+      key: "email",
     },
     {
-      key: 'roles',
-      operation: (value) => value.join(', '),
+      key: "roles",
+      operation: (value) => value.join(", "),
     },
     {
-      key: 'image',
+      key: "image",
       operation: (value) => {
         return value.map((image) => (
           <img
-            style={{ padding: '0.5rem' }}
+            style={{ padding: "0.5rem" }}
             height={60}
             width={75}
             src={image.url}
             alt={image.originalname}
             key={image.public_id}
           />
-        ))
+        ));
       },
     },
-  ]
+  ];
+
+  const filteredData = data?.details?.filter(
+    (user) => user?._id !== auth?.user?._id && user?._id !== isDeletingId
+  );
 
   const handleDelete = async (id) => {
-    setIsDeletingId(id)
-    if (window.confirm('Are you sure?')) {
-      const response = await deleteUser(id)
-      console.log('Response from API:', response)
+    setIsDeletingId(id);
+    if (window.confirm("Are you sure?")) {
+      const response = await deleteUser(id);
+      console.log("Response from API:", response);
       const toastProps = {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 5000,
-      }
+      };
       if (response?.data?.success === true) {
-        toast.success('User deleted successfully!', toastProps)
-        const newData = data.details.filter((user) => user._id !== id)
-        setData({ details: newData })
-      } else toast.error('Failed to delete user.', toastProps)
+        toast.success("User deleted successfully!", toastProps);
+        const newData = data.details.filter((user) => user?._id !== id);
+        setData({ details: newData });
+      } else toast.error("Failed to delete user.", toastProps);
     }
-  }
+  };
 
   const handleEdit = (id) => {
-    navigate(`edit/${id}`)
-  }
+    navigate(`edit/${id}`);
+  };
 
   const actions = [
     {
       onClick: handleEdit,
-      title: 'Edit',
+      title: "Edit",
     },
     {
       onClick: handleDelete,
-      title: 'Delete',
+      title: "Delete",
     },
-  ]
-
-  const filteredData = data?.details?.filter(
-    (user) => user._id !== auth.user._id && user._id !== isDeletingId,
-  )
+  ];
 
   return (
     <>
       <Button
         title="Add User"
         onClick={() => {
-          navigate('/dashboard/user/create')
+          navigate("/dashboard/user/create");
         }}
       />
       {isLoading || isDeleting ? (
@@ -126,5 +120,5 @@ export default function () {
         )
       )}
     </>
-  )
+  );
 }
