75ed52dcad3b7bdcae888450decfd693fbb0b7c6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-01 20:12:07 +0800

    Refactor CameraLayout

--- frontend/src/component/CameraLayout.jsx
+++ frontend/src/component/CameraLayout.jsx
@@ -4,30 +4,35 @@ import CameraImages from "./CameraImages";
 import CameraDetails from "./CameraDetails";
 
 export default function (props) {
-  const { data, onAddToCart } = props;
+  const { data, onAddToCart, cartItems } = props;
 
   return (
     <>
-      {data?.map((detail) => (
-        <Paper key={detail?._id} sx={{ mb: 6 }}>
-          <Box
-            sx={{
-              p: 5,
-              display: "flex",
-              justifyContent: "space-evenly",
-              gap: 5,
-            }}
-          >
-            <CameraImages image={detail?.image} />
-            <CameraDetails
-              name={detail?.name}
-              price={detail?.price}
-              description={detail?.description}
-              onAddToCart={onAddToCart}
-            />
-          </Box>
-        </Paper>
-      ))}
+      {data?.map((detail) => {
+        const isInCart = cartItems.some(
+          (cartItem) => cartItem._id === detail._id
+        );
+
+        return (
+          <Paper key={detail?._id} sx={{ mb: 6 }}>
+            <Box
+              sx={{
+                p: 5,
+                display: "flex",
+                justifyContent: "space-evenly",
+                gap: 5,
+              }}
+            >
+              <CameraImages image={detail?.image} />
+              <CameraDetails
+                item={detail}
+                onAddToCart={() => onAddToCart(detail)}
+                isInCart={isInCart}
+              />
+            </Box>
+          </Paper>
+        );
+      })}
     </>
   );
 }
