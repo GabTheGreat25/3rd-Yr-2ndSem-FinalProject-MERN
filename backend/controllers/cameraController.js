8ca208828f359f3652fb18bbda999696f21877d2
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-05-02 23:28:33 +0800

    Update getall limit to 100

--- backend/controllers/cameraController.js
+++ backend/controllers/cameraController.js
@@ -1,92 +1,92 @@
-const SuccessHandler = require("../utils/successHandler");
-const ErrorHandler = require("../utils/errorHandler");
-const camerasService = require("../services/cameraService");
-const asyncHandler = require("express-async-handler");
-const checkRequiredFields = require("../helpers/checkRequiredFields");
-const { upload } = require("../utils/cloudinary");
+const SuccessHandler = require('../utils/successHandler')
+const ErrorHandler = require('../utils/errorHandler')
+const camerasService = require('../services/cameraService')
+const asyncHandler = require('express-async-handler')
+const checkRequiredFields = require('../helpers/checkRequiredFields')
+const { upload } = require('../utils/cloudinary')
 
 exports.getAllCameras = asyncHandler(async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-  const search = req.query.search;
-  const sort = req.query.sort;
-  const filter = req.query.filter;
+  const page = parseInt(req.query.page) || 1
+  const limit = parseInt(req.query.limit) || 100
+  const search = req.query.search
+  const sort = req.query.sort
+  const filter = req.query.filter
 
   const camerasQuery = camerasService.getAllCamerasData(
     page,
     limit,
     search,
     sort,
-    filter
-  );
-  const cameras = await camerasQuery.lean();
+    filter,
+  )
+  const cameras = await camerasQuery.lean()
 
   if (!cameras.length) {
-    return next(new ErrorHandler("No cameras found"));
+    return next(new ErrorHandler('No cameras found'))
   }
 
-  const cameraNames = cameras.map((u) => u.name).join(", ");
-  const cameraIds = cameras.map((u) => u._id).join(", ");
+  const cameraNames = cameras.map((u) => u.name).join(', ')
+  const cameraIds = cameras.map((u) => u._id).join(', ')
 
   return SuccessHandler(
     res,
     `Cameras with names ${cameraNames} and IDs ${cameraIds} retrieved`,
-    cameras
-  );
-});
+    cameras,
+  )
+})
 
 exports.getSingleCamera = asyncHandler(async (req, res, next) => {
-  const camera = await camerasService.getSingleCameraData(req.params.id);
+  const camera = await camerasService.getSingleCameraData(req.params.id)
 
   return !camera
-    ? next(new ErrorHandler("No camera found"))
+    ? next(new ErrorHandler('No camera found'))
     : SuccessHandler(
         res,
         `Camera ${camera.name} with ID ${camera._id} retrieved`,
-        camera
-      );
-});
+        camera,
+      )
+})
 
 exports.createNewCamera = [
-  upload.array("image"),
-  checkRequiredFields(["user", "name", "text", "price"]),
+  upload.array('image'),
+  checkRequiredFields(['user', 'name', 'text', 'price']),
   asyncHandler(async (req, res, next) => {
-    const camera = await camerasService.CreateCameraData(req);
+    const camera = await camerasService.CreateCameraData(req)
 
     return SuccessHandler(
       res,
       `New camera ${camera.name} created with an ID ${camera._id}`,
-      camera
-    );
+      camera,
+    )
   }),
-];
+]
 
 exports.updateCamera = [
-  upload.array("image"),
-  checkRequiredFields(["user", "name", "text", "price", "image"]),
+  upload.array('image'),
+  checkRequiredFields(['user', 'name', 'text', 'price', 'image']),
   asyncHandler(async (req, res, next) => {
     const camera = await camerasService.updateCameraData(
       req,
       res,
-      req.params.id
-    );
+      req.params.id,
+    )
 
     return SuccessHandler(
       res,
       `Camera ${camera.name} with ID ${camera._id} is updated`,
-      camera
-    );
+      camera,
+    )
   }),
-];
+]
 
 exports.deleteCamera = asyncHandler(async (req, res, next) => {
-  const camera = await camerasService.deleteCameraData(req.params.id);
+  const camera = await camerasService.deleteCameraData(req.params.id)
 
   return !camera
-    ? next(new ErrorHandler("No camera found"))
+    ? next(new ErrorHandler('No camera found'))
     : SuccessHandler(
         res,
         `Camera ${camera.name} with ID ${camera._id} is deleted`,
-        camera
-      );
-});
+        camera,
+      )
+})
