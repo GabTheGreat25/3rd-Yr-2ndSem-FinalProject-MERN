f76b7ee1d66ac3a45f1f32c463abaec4977814b4
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-02 17:10:27 +0800

    Add required in password

--- frontend/src/page/UserLogin/index.jsx
+++ frontend/src/page/UserLogin/index.jsx
@@ -137,6 +137,7 @@ export default function () {
                     id="password"
                     name="password"
                     label="Password"
+                    required
                     fullWidth
                     type={showPassword ? "text" : "password"}
                     autoComplete="secret password"
