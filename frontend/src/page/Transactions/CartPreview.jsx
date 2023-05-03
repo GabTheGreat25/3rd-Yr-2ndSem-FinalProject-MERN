5a70d2988d63198b0ddf7e950d851a15a2727711
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-05-03 09:41:37 +0800

    Add image in CartPreview

--- frontend/src/page/Transactions/CartPreview.jsx
+++ frontend/src/page/Transactions/CartPreview.jsx
@@ -6,7 +6,11 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  CardMedia,
+  colors, // add colors import
 } from '@mui/material'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function ({ cartItems, onRemoveFromCart, onConfirmPurchase }) {
   const [modalOpen, setModalOpen] = useState(false)
@@ -16,33 +20,96 @@ export default function ({ cartItems, onRemoveFromCart, onConfirmPurchase }) {
     setModalOpen(false)
   }
 
+  console.log(cartItems)
+
   return (
-    <div className="cartPreview">
-      <h3>Cart Preview</h3>
+    <div className="cartPreview" style={{ padding: '20px' }}>
+      <h3 style={{ textAlign: 'center', fontSize: '24px', marginTop: '20px' }}>
+        Cart Preview
+      </h3>
       {cartItems && cartItems.length > 0 ? (
         cartItems.map((item) => (
-          <div key={item._id} className="cartItem">
+          <div
+            key={item._id}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
             <span>{item.name}</span>
             <span>{item.price}</span>
-            <button onClick={() => onRemoveFromCart(item)}>Remove</button>
+            {item.image?.map((imageItem, index) => (
+              <CardMedia
+                key={index}
+                component="img"
+                sx={{
+                  height: '100px',
+                  width: '100px',
+                  borderRadius: 2,
+                  margin: '1rem',
+                }}
+                image={imageItem.url}
+                alt={imageItem.alt}
+              />
+            ))}
+            <button
+              style={{ color: 'red' }}
+              onClick={() => onRemoveFromCart(item)}
+            >
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
           </div>
         ))
       ) : (
         <div>No items in cart</div>
       )}
+      <br />
       {cartItems && cartItems.length > 0 ? (
-        <button onClick={() => setModalOpen(true)}>Confirm Purchase</button>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <button
+            style={{
+              backgroundColor: 'blue',
+              borderRadius: '10px',
+              fontSize: '20px',
+              padding: '10px 20px',
+              color: 'white',
+            }}
+            onClick={() => setModalOpen(true)}
+          >
+            Confirm Purchase
+          </button>
+        </div>
       ) : null}
-      <Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
+
+      <Dialog
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        maxWidth="sm"
+        PaperProps={{
+          style: {
+            fontSize: '16px',
+          },
+        }}
+      >
         <DialogTitle>Confirm Purchase</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
+        <DialogContent style={{ padding: '20px' }}>
+          <DialogContentText
+            style={{
+              fontSize: '18px',
+              lineHeight: '1.5',
+            }}
+          >
             Are you sure you want to purchase the selected items?
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setModalOpen(false)}>Cancel</Button>
-          <Button onClick={handleConfirmPurchase}>Confirm</Button>
+          <Button onClick={() => setModalOpen(false)} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmPurchase} color="info">
+            Confirm
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
