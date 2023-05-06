329c5bfabd0fa9b5cf92a02772746ea29e043491
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-07 00:42:13 +0800

    Refactor ShowAllTransaction toastify

--- frontend/src/page/Transactions/ShowAllTransaction.jsx
+++ frontend/src/page/Transactions/ShowAllTransaction.jsx
@@ -1,39 +1,37 @@
-import { useState } from 'react'
-import { DataTable } from '@/component'
+import { useState } from "react";
+import { DataTable } from "@/component";
 import {
   useGetTransactionsQuery,
   useDeleteTransactionMutation,
-} from '@/state/api/reducer'
-import { PacmanLoader } from 'react-spinners'
-import { USER, ERROR } from '../../constants'
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import moment from 'moment-timezone'
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+} from "@/state/api/reducer";
+import { PacmanLoader } from "react-spinners";
+import { USER, ERROR } from "../../constants";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import moment from "moment-timezone";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function () {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const auth = useSelector((state) => state.auth)
+  const auth = useSelector((state) => state.auth);
 
   const { data, isLoading, isError } = useGetTransactionsQuery({
-    populate: ['user', 'cameras'],
-  })
+    populate: ["user", "cameras"],
+  });
 
-  const [isDeletingId, setIsDeletingId] = useState(null)
+  const [isDeletingId, setIsDeletingId] = useState(null);
 
-  const [
-    deleteTransaction,
-    { isLoading: isDeleting, isError: isDeleteError },
-  ] = useDeleteTransactionMutation()
+  const [deleteTransaction, { isLoading: isDeleting, isError: isDeleteError }] =
+    useDeleteTransactionMutation();
 
-  const headers = ['ID', 'Customer', 'Cameras', 'Status', 'Date']
+  const headers = ["ID", "Customer", "Cameras", "Status", "Date"];
 
   const keys = [
     {
-      key: '_id',
+      key: "_id",
       operation: (value, row) => (
         <Link to={`/dashboard/transaction/${row?._id}`} className="link">
           {row?._id}
@@ -41,63 +39,64 @@ export default function () {
       ),
     },
     {
-      key: 'user',
-      operation: (value) => (value ? value?.name : ''),
+      key: "user",
+      operation: (value) => (value ? value?.name : ""),
     },
     {
-      key: 'cameras',
-      operation: (value) => value?.map((camera) => camera?.name).join(', '),
+      key: "cameras",
+      operation: (value) => value?.map((camera) => camera?.name).join(", "),
     },
     {
-      key: 'status',
+      key: "status",
     },
     {
-      key: 'date',
+      key: "date",
       operation: (value) =>
-        moment(value).tz('Asia/Manila').format('YYYY-MM-DD'),
+        moment(value).tz("Asia/Manila").format("YYYY-MM-DD"),
     },
-  ]
+  ];
 
   const filteredData = data?.details?.filter(
-    (transaction) => transaction?._id !== isDeletingId,
-  )
+    (transaction) => transaction?._id !== isDeletingId
+  );
 
   const handleDelete = async (id) => {
-    setIsDeletingId(id)
-    if (window.confirm('Are you sure?')) {
-      const response = await deleteTransaction(id)
+    setIsDeletingId(id);
+    if (window.confirm("Are you sure?")) {
+      const response = await deleteTransaction(id);
 
       const toastProps = {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 5000,
-      }
+      };
       if (response?.data?.success === true) {
-        toast.success('Transaction deleted successfully!', toastProps)
+        toast.success(`${response?.data?.message}`, toastProps);
         const newData = data?.details?.filter(
-          (transaction) => transaction?._id !== id,
-        )
-        setData({ details: newData })
-      } else toast.error('Failed to delete transaction.', toastProps)
+          (transaction) => transaction?._id !== id
+        );
+        setData({ details: newData });
+      } else
+        toast.error(`${response?.error?.data?.error?.message}`, toastProps);
     }
-  }
+  };
   const handleEdit = (id) => {
-    navigate(`edit/${id}`)
-  }
+    navigate(`edit/${id}`);
+  };
 
   const actions = [
     {
       onClick: handleEdit,
-      title: 'Edit',
+      title: "Edit",
     },
     ...(auth?.user?.roles?.includes(USER.ADMIN)
       ? [
           {
             onClick: handleDelete,
-            title: 'Delete',
+            title: "Delete",
           },
         ]
       : []),
-  ]
+  ];
 
   return (
     <>
@@ -120,5 +119,5 @@ export default function () {
         )
       )}
     </>
-  )
+  );
 }
