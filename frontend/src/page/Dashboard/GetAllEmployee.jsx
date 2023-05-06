c63576b4991405feab41cbe9ae8cd44e1de80784
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-06 21:33:01 +0800

    Refactor GetAllEmployee

--- frontend/src/page/Dashboard/GetAllEmployee.jsx
+++ frontend/src/page/Dashboard/GetAllEmployee.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ListData from "../../component/ListData";
 import { PersonPin } from "@mui/icons-material";
 import { useGetUsersQuery } from "@/state/api/reducer";
-import { USER } from "@/constants";
+import { USER, ERROR } from "@/constants";
 import { PacmanLoader } from "react-spinners";
 
 export default function () {
-  const { data, isLoading } = useGetUsersQuery();
+  const { data, isLoading, isError } = useGetUsersQuery();
   const users = data?.details ?? [];
   const employees = users.filter((user) => user.roles.includes(USER.EMPLOYEE));
   const employeeCount = employees.length;
@@ -15,6 +15,8 @@ export default function () {
     <div className="loader">
       <PacmanLoader color="#2c3e50" loading={true} size={50} />
     </div>
+  ) : isError ? (
+    <div className="errorMessage">{ERROR.GET_USERS_ERROR}</div>
   ) : (
     <ListData
       title="Employee"
