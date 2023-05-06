5f0a03203dbd4a1d7dfa54cb7aa8c4d55122012d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-06 18:36:37 +0800

    Remove try and catch and add constants

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,34 +1,30 @@
 const { verifyAccessToken } = require("../utils/token");
 const ErrorHandler = require("../utils/errorHandler");
 const { getBlacklistedTokens } = require("../services/userService");
+const { STATUSCODE } = require("../constants/index");
 
 exports.verifyJWT = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!authHeader?.match(/^Bearer\s+(.*)$/))
-      throw new ErrorHandler("Please Log In First");
+  if (!authHeader?.match(/^Bearer\s+(.*)$/))
+    throw new ErrorHandler("Please Log In First");
 
-    const token = authHeader?.match(/^Bearer\s+(.*)$/)[1];
+  const token = authHeader?.match(/^Bearer\s+(.*)$/)[1];
 
-    if (getBlacklistedTokens().includes(token)) {
-      throw new ErrorHandler("Invalid Token");
-    }
+  if (getBlacklistedTokens().includes(token))
+    throw new ErrorHandler("Token Already Expired");
 
-    const decoded = verifyAccessToken(token);
-    req.user = decoded?.UserInfo?.email;
-    req.roles = decoded?.UserInfo?.roles;
+  const decoded = verifyAccessToken(token);
+  req.user = decoded?.UserInfo?.email;
+  req.roles = decoded?.UserInfo?.roles;
 
-    next();
-  } catch (error) {
-    next(error);
-  }
+  next();
 };
 
 exports.authorizeRoles =
   (...allowedRoles) =>
   (req, res, next) =>
-    allowedRoles.length === 0 || !req.roles
+    allowedRoles.length === STATUSCODE.ZERO || !req.roles
       ? next()
       : req.roles.some((role) => allowedRoles.includes(role))
       ? next()
