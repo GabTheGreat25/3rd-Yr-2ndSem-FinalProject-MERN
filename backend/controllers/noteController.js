3ea102bb71a2501f4fbdc69ecb506614c95f0e34
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-06 18:35:07 +0800

    Refactor noteController

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,85 +1,82 @@
-const SuccessHandler = require('../utils/successHandler')
-const ErrorHandler = require('../utils/errorHandler')
-const notesService = require('../services/noteService')
-const asyncHandler = require('express-async-handler')
-const checkRequiredFields = require('../helpers/checkRequiredFields')
+const SuccessHandler = require("../utils/successHandler");
+const ErrorHandler = require("../utils/errorHandler");
+const notesService = require("../services/noteService");
+const asyncHandler = require("express-async-handler");
+const checkRequiredFields = require("../helpers/checkRequiredFields");
+const { STATUSCODE } = require("../constants/index");
 
 exports.getAllNotes = asyncHandler(async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1
-  const limit = parseInt(req.query.limit) || 100
-  const search = req.query.search
-  const sort = req.query.sort
-  const filter = req.query.filter
+  const page = parseInt(req.query.page) || STATUSCODE.ONE;
+  const limit = parseInt(req.query.limit) || STATUSCODE.HUNDRED;
+  const search = req.query.search;
+  const sort = req.query.sort;
+  const filter = req.query.filter;
 
-  const notesQuery = notesService.getAllNotesData(
+  const notes = await notesService.getAllNotesData(
     page,
     limit,
     search,
     sort,
-    filter,
-  )
-  const notes = await notesQuery
+    filter
+  );
 
-  if (!notes.length) {
-    return next(new ErrorHandler('No notes found'))
-  }
-
-  const noteTitles = notes.map((u) => u.title).join(', ')
-  const noteIds = notes.map((u) => u._id).join(', ')
-
-  return SuccessHandler(
-    res,
-    `Notes with titles ${noteTitles} and IDs ${noteIds} retrieved`,
-    notes,
-  )
-})
+  return notes?.length === STATUSCODE.ZERO
+    ? next(new ErrorHandler("No notes found"))
+    : SuccessHandler(
+        res,
+        `Notes with titles ${notes
+          .map((u) => u.title)
+          .join(", ")} and IDs ${notes.map((u) => u._id).join(", ")} retrieved`,
+        notes
+      );
+});
 
 exports.getSingleNote = asyncHandler(async (req, res, next) => {
-  const note = await notesService.getSingleNoteData(req.params.id)
+  const note = await notesService.getSingleNoteData(req.params.id);
 
   return !note
-    ? next(new ErrorHandler('No note found'))
+    ? next(new ErrorHandler("No note found"))
     : SuccessHandler(
         res,
         `Note ${note.title} with ID ${note._id} retrieved`,
-        note,
-      )
-})
+        note
+      );
+});
 
 exports.createNewNote = [
-  checkRequiredFields(['user', 'title', 'text']),
+  checkRequiredFields(["user", "title", "text"]),
   asyncHandler(async (req, res, next) => {
-    const note = await notesService.CreateNoteData(req)
+    const note = await notesService.CreateNoteData(req);
 
     return SuccessHandler(
       res,
       `New note ${note.title} created with an ID ${note._id}`,
-      note,
-    )
+      note
+    );
   }),
-]
+];
 
 exports.updateNote = [
-  checkRequiredFields(['user', 'title', 'text']),
+  checkRequiredFields(["user", "title", "text"]),
   asyncHandler(async (req, res, next) => {
-    const note = await notesService.updateNoteData(req, res, req.params.id)
+    const note = await notesService.updateNoteData(req, res, req.params.id);
 
     return SuccessHandler(
       res,
       `Note ${note.title} with ID ${note._id} is updated`,
-      note,
-    )
+      note
+    );
   }),
-]
+];
 
 exports.deleteNote = asyncHandler(async (req, res, next) => {
-  const note = await notesService.deleteNoteData(req.params.id)
+  const note = await notesService.deleteNoteData(req.params.id);
 
   return !note
-    ? next(new ErrorHandler('No note found'))
+    ? next(new ErrorHandler("No note found"))
     : SuccessHandler(
         res,
         `Note ${note.title} with ID ${note._id} is deleted`,
-        note,
-      )
-})
+        note
+      );
+});
