440c76381d85b5d75f75ac6a36b228af6faa1c02
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-07 00:41:20 +0800

    Refactor Reset Password toastify

--- frontend/src/page/ResetPassword/index.jsx
+++ frontend/src/page/ResetPassword/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   Button,
   Container,
@@ -6,67 +6,57 @@ import {
   Typography,
   InputAdornment,
   IconButton,
-} from '@mui/material'
-import { useResetPasswordMutation } from '../../state/api/reducer'
-import { Box } from '@mui/system'
-import { useFormik } from 'formik'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
-import { resetPasswordValidation } from '../../validation'
-import { PacmanLoader } from 'react-spinners'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { TAGS } from '../../constants'
+} from "@mui/material";
+import { useResetPasswordMutation } from "../../state/api/reducer";
+import { Box } from "@mui/system";
+import { useFormik } from "formik";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { resetPasswordValidation } from "../../validation";
+import { PacmanLoader } from "react-spinners";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { TAGS } from "../../constants";
 
 export default function () {
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
-  const [resetPassword, { isLoading }] = useResetPasswordMutation()
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [resetPassword, { isLoading }] = useResetPasswordMutation();
 
   const formik = useFormik({
     initialValues: {
-      newPassword: '',
-      confirmPassword: '',
+      newPassword: "",
+      confirmPassword: "",
     },
     validationSchema: resetPasswordValidation,
     onSubmit: (values) => {
-      const { newPassword, confirmPassword } = values
-      const email = new URLSearchParams(window.location.search).get(TAGS.EMAIL)
+      const { newPassword, confirmPassword } = values;
+      const email = new URLSearchParams(window.location.search).get(TAGS.EMAIL);
       resetPassword({
         newPassword,
         confirmPassword,
         email,
-      })
-        .then((response) => {
-          const toastProps = {
-            position: toast.POSITION.TOP_RIGHT,
-            autoClose: 5000,
-          }
-          if (response?.data?.success === true) {
-            window.open(
-              `https://mailtrap.io/inboxes/1656145/messages`,
-              '_blank',
-            )
-            toast.success('Emailed Reset Password successfully!', toastProps)
-          } else {
-            toast.error("Email wasn't sent to the user.", toastProps)
-          }
-        })
-        .catch((error) => {
-          toast.error("Email wasn't sent to the user.", {
-            position: toast.POSITION.TOP_RIGHT,
-            autoClose: 5000,
-          })
-        })
+      }).then((response) => {
+        const toastProps = {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 5000,
+        };
+        if (response?.data?.success === true) {
+          window.open(`https://mailtrap.io/inboxes`, "_blank");
+          toast.success(`${response?.data?.message}`, toastProps);
+        } else {
+          toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+        }
+      });
     },
-  })
+  });
 
   const handleClickShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
+    setShowPassword(!showPassword);
+  };
 
   const handleClickShowConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword)
-  }
+    setShowConfirmPassword(!showConfirmPassword);
+  };
 
   return (
     <>
@@ -80,9 +70,9 @@ export default function () {
             <Box
               sx={{
                 mt: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
               }}
             >
               <Typography variant="h4" gutterBottom>
@@ -91,7 +81,7 @@ export default function () {
               <form onSubmit={formik.handleSubmit}>
                 <TextField
                   label="New Password"
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   name="newPassword"
                   value={formik.values.newPassword}
                   onChange={formik.handleChange}
@@ -119,7 +109,7 @@ export default function () {
                 />
                 <TextField
                   label="Confirm Password"
-                  type={showConfirmPassword ? 'text' : 'password'}
+                  type={showConfirmPassword ? "text" : "password"}
                   name="confirmPassword"
                   value={formik.values.confirmPassword}
                   onChange={formik.handleChange}
@@ -164,5 +154,5 @@ export default function () {
         </>
       )}
     </>
-  )
+  );
 }
