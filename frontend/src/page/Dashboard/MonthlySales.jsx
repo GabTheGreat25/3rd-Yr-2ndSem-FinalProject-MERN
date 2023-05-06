55c69cb799b6d762769d9bebc8014e21013e3f6d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-06 21:33:17 +0800

    Refactor MonthlySales

--- frontend/src/page/Dashboard/MonthlySales.jsx
+++ frontend/src/page/Dashboard/MonthlySales.jsx
@@ -8,24 +8,25 @@ import {
   YAxis,
 } from "recharts";
 import { useGetTransactionsQuery } from "@/state/api/reducer";
+import { ERROR } from "@/constants";
+import { PacmanLoader } from "react-spinners";
 
 export default function MonthlySalesChart() {
-  const { data, isLoading } = useGetTransactionsQuery();
-  if (isLoading) return <div>Loading...</div>;
+  const { data, isLoading, isError } = useGetTransactionsQuery();
 
   const monthNames = [
-    "January",
-    "February",
+    "Jan",
+    "Feb",
     "March",
     "April",
     "May",
     "June",
     "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
 
   const groupedData = data?.details
@@ -42,32 +43,42 @@ export default function MonthlySalesChart() {
       }, {})
     : {};
 
-  const chartData = monthNames.map((monthName, index) => ({
+  const chartData = monthNames?.map((monthName, index) => ({
     month: monthName,
     sales: groupedData[index] || 0,
   }));
 
-  if (!data || !data.success || groupedData.length === 0) return null;
-
   return (
-    <AreaChart data={chartData} width={1400} height={400}>
-      <defs>
-        <linearGradient id="colorSales" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#33B2DF" stopOpacity={0.8} />
-          <stop offset="95%" stopColor="#33B2DF" stopOpacity={0} />
-        </linearGradient>
-      </defs>
-      <XAxis dataKey="month" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Area
-        type="monotone"
-        dataKey="sales"
-        stroke="#33B2DF"
-        fillOpacity={1}
-        fill="url(#colorSales)"
-      />
-    </AreaChart>
+    <>
+      {isLoading ? (
+        <div className="loader">
+          <PacmanLoader color="#2c3e50" loading={true} size={50} />
+        </div>
+      ) : isError ? (
+        <div className="errorMessage">{ERROR.GET_TRANSACTIONS_ERROR}</div>
+      ) : groupedData.length === 0 || !data.success ? null : (
+        <>
+          <AreaChart data={chartData} width={1200} height={400}>
+            <defs>
+              <linearGradient id="colorSales" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#33B2DF" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#33B2DF" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="sales"
+              stroke="#33B2DF"
+              fillOpacity={1}
+              fill="url(#colorSales)"
+            />
+          </AreaChart>
+        </>
+      )}
+    </>
   );
 }
