9ad8e13acba1fed5350e3c63490f834d80bbaa3b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-07 00:41:07 +0800

    Refactor Notes index toastify

--- frontend/src/page/Notes/index.jsx
+++ frontend/src/page/Notes/index.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import { DataTable, Button } from '@/component'
-import { useGetNotesQuery, useDeleteNoteMutation } from '@/state/api/reducer'
-import { PacmanLoader } from 'react-spinners'
-import { USER, ERROR } from '../../constants'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { useState } from "react";
+import { DataTable, Button } from "@/component";
+import { useGetNotesQuery, useDeleteNoteMutation } from "@/state/api/reducer";
+import { PacmanLoader } from "react-spinners";
+import { USER, ERROR } from "../../constants";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function () {
-  const navigate = useNavigate()
-  const { data, isLoading, isError: isNotesError } = useGetNotesQuery({
-    populate: 'user',
-  })
+  const navigate = useNavigate();
+  const {
+    data,
+    isLoading,
+    isError: isNotesError,
+  } = useGetNotesQuery({
+    populate: "user",
+  });
 
-  const [isDeletingId, setIsDeletingId] = useState(null)
+  const [isDeletingId, setIsDeletingId] = useState(null);
 
-  const auth = useSelector((state) => state.auth)
+  const auth = useSelector((state) => state.auth);
 
-  const [
-    deleteNote,
-    { isLoading: isDeleting, isError: isDeleteError },
-  ] = useDeleteNoteMutation()
+  const [deleteNote, { isLoading: isDeleting, isError: isDeleteError }] =
+    useDeleteNoteMutation();
 
-  const headers = ['ID', 'Title', 'Text', 'Completed', 'Employee']
+  const headers = ["ID", "Title", "Text", "Completed", "Employee"];
 
   const keys = [
     {
-      key: '_id',
+      key: "_id",
       operation: (value, row) => (
         <Link to={`/dashboard/note/${row?._id}`} className="link">
           {row?._id}
@@ -36,60 +38,61 @@ export default function () {
       ),
     },
     {
-      key: 'title',
+      key: "title",
     },
     {
-      key: 'text',
+      key: "text",
     },
     {
-      key: 'completed',
-      operation: (value) => (value ? 'Yes' : 'No'),
+      key: "completed",
+      operation: (value) => (value ? "Yes" : "No"),
     },
     {
-      key: 'user',
-      operation: (value) => (value ? value?.name : ''),
+      key: "user",
+      operation: (value) => (value ? value?.name : ""),
     },
-  ]
+  ];
 
   const filteredData = data?.details?.filter(
-    (note) => note?._id !== isDeletingId,
-  )
+    (note) => note?._id !== isDeletingId
+  );
 
   const handleDelete = async (id) => {
-    setIsDeletingId(id)
-    if (window.confirm('Are you sure?')) {
-      const response = await deleteNote(id)
+    setIsDeletingId(id);
+    if (window.confirm("Are you sure?")) {
+      const response = await deleteNote(id);
 
       const toastProps = {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 5000,
-      }
+      };
       if (response?.data?.success === true) {
-        toast.success('Note deleted successfully!', toastProps)
-        const newData = data?.details?.filter((note) => note?._id !== id)
-        setData({ details: newData })
-      } else toast.error('Failed to delete note.', toastProps)
+        toast.success(`${response?.data?.message}`, toastProps);
+        const newData = data?.details?.filter((note) => note?._id !== id);
+        setData({ details: newData });
+      } else
+        toast.error(`${response?.error?.data?.error?.message}`, toastProps);
     }
-  }
+  };
 
   const handleEdit = (id) => {
-    navigate(`edit/${id}`)
-  }
+    navigate(`edit/${id}`);
+  };
 
   const actions = [
     {
       onClick: handleEdit,
-      title: 'Edit',
+      title: "Edit",
     },
     ...(auth?.user?.roles?.includes(USER.ADMIN)
       ? [
           {
             onClick: handleDelete,
-            title: 'Delete',
+            title: "Delete",
           },
         ]
       : []),
-  ]
+  ];
 
   return (
     <>
@@ -97,7 +100,7 @@ export default function () {
         <Button
           title="Add Note"
           onClick={() => {
-            navigate('/dashboard/note/create')
+            navigate("/dashboard/note/create");
           }}
         />
       )}
@@ -120,5 +123,5 @@ export default function () {
         )
       )}
     </>
-  )
+  );
 }
