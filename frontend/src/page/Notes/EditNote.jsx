12ab7e6e998c1381d9902c2e94ee783c1330a326
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-07 00:40:59 +0800

    Refactor EditNote toastify

--- frontend/src/page/Notes/EditNote.jsx
+++ frontend/src/page/Notes/EditNote.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   TextField,
   Typography,
@@ -9,69 +9,72 @@ import {
   MenuItem,
   FormControlLabel,
   Checkbox,
-} from '@mui/material'
+} from "@mui/material";
 import {
   useUpdateNoteMutation,
   useGetNoteByIdQuery,
   useGetUsersQuery,
-} from '@/state/api/reducer'
-import { useFormik } from 'formik'
-import { editNoteValidation } from '../../validation'
-import { useNavigate, useParams } from 'react-router-dom'
-import { USER, ERROR } from '../../constants'
-import { PacmanLoader } from 'react-spinners'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+} from "@/state/api/reducer";
+import { useFormik } from "formik";
+import { editNoteValidation } from "../../validation";
+import { useNavigate, useParams } from "react-router-dom";
+import { USER, ERROR } from "../../constants";
+import { PacmanLoader } from "react-spinners";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 
 export default function () {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const { id } = useParams()
+  const { id } = useParams();
 
-  const { data, isLoading, isError } = useGetNoteByIdQuery(id)
+  const { data, isLoading, isError } = useGetNoteByIdQuery(id);
 
-  const { data: getAllNote } = useGetUsersQuery()
-  const users = getAllNote?.details ?? []
+  const auth = useSelector((state) => state.auth);
+
+  const isAdmin = auth?.user?.roles?.includes("Admin");
+  const isEmployee =
+    auth?.user?.roles?.includes("Employee") && auth?.user?.roles?.length === 1;
+
+  const { data: getAllNote } = useGetUsersQuery();
+  const users = getAllNote?.details ?? [];
   const employees = users?.filter((user) =>
-    user?.roles?.includes(USER.EMPLOYEE),
-  )
+    user?.roles?.includes(USER.EMPLOYEE)
+  );
   const associatedUser = users?.find(
-    (user) => user?._id === data?.details?.user?._id,
-  )
+    (user) => user?._id === data?.details?.user?._id
+  );
 
-  const [updateNote] = useUpdateNoteMutation()
+  const [updateNote] = useUpdateNoteMutation();
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
-      title: data?.details?.title || '',
-      text: data?.details?.text || '',
-      user: associatedUser?._id || '',
-      completed: data?.details?.completed || false,
+      title: data?.details?.title || "",
+      text: data?.details?.text || "",
+      user: associatedUser?._id || "",
+      completed: Boolean(data?.details?.completed),
     },
     validationSchema: editNoteValidation,
     onSubmit: (values) => {
-      updateNote({ id: data?.details?._id, payload: values })
-        .then((response) => {
+      updateNote({ id: data?.details?._id, payload: values }).then(
+        (response) => {
           const toastProps = {
             position: toast.POSITION.TOP_RIGHT,
             autoClose: 5000,
-          }
+          };
+          console.log(response);
           if (response?.data?.success === true) {
-            navigate('/dashboard/note')
-            toast.success('Note edited successfully!', toastProps)
+            navigate("/dashboard/note");
+            toast.success(`${response?.data?.message}`, toastProps);
           } else {
-            toast.error('Error while editing note.', toastProps)
+            toast.error(`${response?.error?.data?.error?.message}`, toastProps);
           }
-        })
-        .catch((error) => {
-          toast.error('Error while editing note.', {
-            position: toast.POSITION.TOP_RIGHT,
-            autoClose: 5000,
-          })
-        })
+        }
+      );
     },
-  })
+  });
 
   return (
     <>
@@ -120,59 +123,64 @@ export default function () {
                   helperText={formik.touched.text && formik.errors.text}
                 />
               </Grid>
-              <Grid item xs={12}>
-                <InputLabel id="user-label">Select User</InputLabel>
-                <Select
-                  labelId="user-label"
-                  id="user"
-                  name="user"
-                  fullWidth
-                  value={formik.values.user}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.user && Boolean(formik.errors.user)}
-                  displayEmpty
-                >
-                  <MenuItem value="" disabled>
-                    Please select an employee
-                  </MenuItem>
-                  {Array.isArray(employees) &&
-                    employees.map((user) => {
-                      return (
-                        <MenuItem key={user._id} value={user._id}>
-                          {user.name}
-                        </MenuItem>
-                      )
-                    })}
-                </Select>
-                {formik.touched.user && formik.errors.user && (
-                  <Typography variant="caption" color="error">
-                    {formik.errors.user}
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={formik.values.completed}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                      name="completed"
-                      id="completed"
-                      color="primary"
-                    />
-                  }
-                  label="Check This If You Completed Your Task"
-                />
-              </Grid>
+              {isAdmin && (
+                <Grid item xs={12}>
+                  <InputLabel id="user-label">Select User</InputLabel>
+                  <Select
+                    labelId="user-label"
+                    id="user"
+                    name="user"
+                    fullWidth
+                    value={formik.values.user}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.user && Boolean(formik.errors.user)}
+                    displayEmpty
+                  >
+                    <MenuItem value="" disabled>
+                      Please select an employee
+                    </MenuItem>
+                    {Array.isArray(employees) &&
+                      employees.map((user) => {
+                        return (
+                          <MenuItem key={user._id} value={user._id}>
+                            {user.name}
+                          </MenuItem>
+                        );
+                      })}
+                  </Select>
+                  {formik.touched.user && formik.errors.user && (
+                    <Typography variant="caption" color="error">
+                      {formik.errors.user}
+                    </Typography>
+                  )}
+                </Grid>
+              )}
+              {isEmployee && (
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={formik.values.completed}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.completed.toString()}
+                        name="completed"
+                        id="completed"
+                        color="primary"
+                      />
+                    }
+                    label="Check This If You Completed Your Task"
+                  />
+                </Grid>
+              )}
             </Grid>
             <Button
               variant="contained"
               color="primary"
               type="submit"
               disabled={!formik.isValid}
-              sx={{ mt: '1rem' }}
+              sx={{ mt: "1rem" }}
             >
               Submit
             </Button>
@@ -180,5 +188,5 @@ export default function () {
         </>
       )}
     </>
-  )
+  );
 }
