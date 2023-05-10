888bb2fe1b913ecafdd8b2534634a749d989b158
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:19:49 +0800

    Refactor GetPerCamera

--- frontend/src/page/Cameras/GetPerCameras.jsx
+++ frontend/src/page/Cameras/GetPerCameras.jsx
@@ -1,50 +1,56 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useGetCameraByIdQuery } from '@/state/api/reducer'
-import { Card, CardContent, Typography } from '@mui/material'
-import { ERROR } from '../../constants'
-import { PacmanLoader } from 'react-spinners'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetCameraByIdQuery } from "@api";
+import { Card, CardContent, Typography } from "@mui/material";
+import { ERROR, TAGS, RESOURCE } from "@/constants";
+import { PacmanLoader } from "react-spinners";
 
 export default function () {
-  const { id } = useParams()
+  const { id } = useParams();
   const { data, isLoading, isError } = useGetCameraByIdQuery(id, {
-    populate: 'user',
-  })
+    populate: TAGS.USER,
+  });
 
-  const { name, text, price, image } = data?.details ?? {}
+  const { name, text, price, image } = data?.details ?? {};
 
   return (
     <>
       {isLoading ? (
         <div className="loader">
-          <PacmanLoader color="#2c3e50" loading={true} size={50} />
+          <PacmanLoader
+            color="#2c3e50"
+            loading={true}
+            size={RESOURCE.NUMBER.FIFTY}
+          />
         </div>
       ) : isError ? (
         <div className="errorMessage">{ERROR.GET_CAMERAS_ERROR}</div>
       ) : (
         <Card
           sx={{
-            margin: 'auto',
-            width: '50%',
-            textAlign: 'center',
-            backgroundColor: '#2c3e50',
-            color: '#f1f2f6',
-            borderRadius: '.75rem',
+            margin: "auto",
+            width: "50%",
+            textAlign: "center",
+            backgroundColor: "#2c3e50",
+            color: "#f1f2f6",
+            borderRadius: ".75rem",
           }}
         >
           <CardContent>
             <Typography variant="h5" component="div">
               Camera Details
             </Typography>
-            <Typography sx={{ mb: 1.5 }}>ID: {id}</Typography>
+            <Typography sx={{ mb: RESOURCE.NUMBER.ONE_POINT_FIVE }}>
+              ID: {id}
+            </Typography>
             {image?.map((image) => (
               <img
-                style={{ padding: '0 .5rem' }}
-                height={150}
-                width={150}
-                src={image.url}
-                alt={image.originalname}
-                key={image.public_id}
+                style={{ padding: "0 .5rem" }}
+                height={RESOURCE.NUMBER.HUNDRED_FIFTY}
+                width={RESOURCE.NUMBER.HUNDRED_FIFTY}
+                src={image?.url}
+                alt={image?.originalname}
+                key={image?.public_id}
               />
             ))}
             <Typography variant="body2">
@@ -60,5 +66,5 @@ export default function () {
         </Card>
       )}
     </>
-  )
+  );
 }
