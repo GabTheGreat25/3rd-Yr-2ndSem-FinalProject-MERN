7f5e27f2f75962557d3871aff0c656f928fa7b28
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:22:41 +0800

    Refactor TotalProfitPerYear

--- frontend/src/page/Dashboard/TotalProfitPerYear.jsx
+++ frontend/src/page/Dashboard/TotalProfitPerYear.jsx
@@ -7,25 +7,35 @@ import {
   CartesianGrid,
   Tooltip,
 } from "recharts";
-import { useGetTransactionsQuery } from "@/state/api/reducer";
+import { useGetTransactionsQuery } from "@api";
 import { PacmanLoader } from "react-spinners";
+import { RESOURCE } from "@/constants";
 
 export default function () {
   const { data, isLoading } = useGetTransactionsQuery();
 
   const transactionsWithTotalSales =
-    data?.details?.map((transaction) => {
-      const totalSales = transaction.cameras.reduce((acc, camera) => {
-        return acc + camera.price;
-      }, 0);
-      return { ...transaction, totalSales };
-    }) || [];
+    data?.details
+      ?.map((transaction) => {
+        if (
+          transaction.status !== RESOURCE.PENDING &&
+          transaction.status !== RESOURCE.CANCELLED &&
+          transaction.status !== RESOURCE.NOT_PAID
+        ) {
+          const totalSales = transaction?.cameras?.reduce((acc, camera) => {
+            return acc + camera?.price;
+          }, RESOURCE.NUMBER.ZERO);
+          return { ...transaction, totalSales };
+        }
+        return null;
+      })
+      .filter(Boolean) || [];
 
-  const groupedData = transactionsWithTotalSales.reduce((acc, transaction) => {
+  const groupedData = transactionsWithTotalSales?.reduce((acc, transaction) => {
     const year = new Date(transaction.date).getFullYear();
-    const sales = transaction.totalSales || 0;
+    const sales = transaction.totalSales || RESOURCE.NUMBER.ZERO;
 
-    acc[year] = (acc[year] || 0) + sales;
+    acc[year] = (acc[year] || RESOURCE.NUMBER.ZERO) + sales;
     return acc;
   }, {});
 
@@ -38,30 +48,42 @@ export default function () {
     <>
       {isLoading ? (
         <div className="loader">
-          <PacmanLoader color="#2c3e50" loading={true} size={50} />
+          <PacmanLoader
+            color="#2c3e50"
+            loading={true}
+            size={RESOURCE.NUMBER.FIFTY}
+          />
         </div>
-      ) : (
+      ) : !data ? null : (
         <>
-          {groupedData.length !== 0 && (
+          {groupedData.length !== RESOURCE.NUMBER.ZERO && (
             <LineChart
-              width={600}
-              height={400}
+              width={RESOURCE.NUMBER.SIX_HUNDRED}
+              height={RESOURCE.NUMBER.FOUR_HUNDRED}
               data={chartData}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              margin={{
+                top: RESOURCE.NUMBER.FIVE,
+                right: RESOURCE.NUMBER.THIRTY,
+                left: RESOURCE.NUMBER.TWENTY,
+                bottom: RESOURCE.NUMBER.FIVE,
+              }}
             >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="year" />
               <YAxis />
               <Tooltip
-                formatter={(value, name) => ["$" + value, "Total Sales"]}
+                formatter={(value, name) => [
+                  value + RESOURCE.PHP,
+                  "Total Sales",
+                ]}
               />
               <Line
                 type="monotone"
                 dataKey="sales"
                 stroke="#8884d8"
-                strokeWidth={2}
-                dot={{ strokeWidth: 0 }}
-                activeDot={{ r: 8 }}
+                strokeWidth={RESOURCE.NUMBER.TWO}
+                dot={{ strokeWidth: RESOURCE.NUMBER.ZERO }}
+                activeDot={{ r: RESOURCE.NUMBER.EIGHT }}
               />
             </LineChart>
           )}
