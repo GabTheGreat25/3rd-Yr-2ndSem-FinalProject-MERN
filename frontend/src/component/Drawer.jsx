359e475efbdaf67360c8e7252f15d369c1d9703c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:16:32 +0800

    Refactor Drawer

--- frontend/src/component/Drawer.jsx
+++ frontend/src/component/Drawer.jsx
@@ -1,30 +1,31 @@
 import { styled } from "@mui/material/styles";
 import MuiDrawer from "@mui/material/Drawer";
+import { RESOURCE } from "@/constants";
 
 export default styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== RESOURCE.OPEN,
 })(({ theme, open }) => ({
   "& .MuiDrawer-paper": {
     position: "relative",
     whiteSpace: "nowrap",
-    width: 240,
-    transition: theme.transitions.create("width", {
+    width: RESOURCE.NUMBER.TWO_HUNDRED_FORTY,
+    transition: theme.transitions.create(RESOURCE.WIDTH, {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
     boxSizing: "border-box",
     ...(!open && {
       overflowX: "hidden",
-      transition: theme.transitions.create("width", {
+      transition: theme.transitions.create(RESOURCE.WIDTH, {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
-      width: theme.spacing(7),
+      width: theme.spacing(RESOURCE.NUMBER.SEVEN),
       [theme.breakpoints.up("sm")]: {
-        width: theme.spacing(9),
+        width: theme.spacing(RESOURCE.NUMBER.NINE),
       },
     }),
-    zIndex: 1,
+    zIndex: RESOURCE.NUMBER.ONE,
     backgroundColor: "#f1f2f6",
   },
 }));
