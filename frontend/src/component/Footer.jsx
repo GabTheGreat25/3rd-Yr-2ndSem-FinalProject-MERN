9057c51a1f16aab507b7aab5c698e2d5ff9789cd
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:16:38 +0800

    Refactor Footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -3,10 +3,18 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MuiAppBar from "@mui/material/AppBar";
+import { RESOURCE } from "@/constants";
 
 export default function () {
   return (
-    <footer style={{ position: "fixed", bottom: 0, width: "100%", zIndex: 2 }}>
+    <footer
+      style={{
+        position: "fixed",
+        bottom: RESOURCE.NUMBER.ZERO,
+        width: "100%",
+        zIndex: RESOURCE.NUMBER.TWO,
+      }}
+    >
       <MuiAppBar position="static" sx={{ backgroundColor: "#2c3e50" }}>
         <Toolbar sx={{ justifyContent: "center" }}>
           <Typography variant="body2" color="inherit">
