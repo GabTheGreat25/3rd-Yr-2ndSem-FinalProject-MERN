4db558a57f14c299dde7da727e5affea0143305c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 12:26:59 +0800

    Remove origin from be

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
   "http://localhost:6969",
-  "https://3rd-yr-2nd-sem-final-project-mern-backend.vercel.app/",
   "https://www.camrentals.com",
   "https://camrentals.com",
 ];
