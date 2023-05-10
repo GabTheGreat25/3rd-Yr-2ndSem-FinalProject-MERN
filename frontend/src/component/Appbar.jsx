6ac2478ad6412ffa1dcf77e760d4c5d03916fa7a
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:15:43 +0800

    Refactor Appbar

--- frontend/src/component/Appbar.jsx
+++ frontend/src/component/Appbar.jsx
@@ -1,18 +1,19 @@
 import { styled } from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
+import { RESOURCE } from "@/constants";
 
 export default styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== RESOURCE.OPEN,
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
+  zIndex: theme.zIndex.drawer + RESOURCE.NUMBER.ONE,
+  transition: theme.transitions.create([RESOURCE.WIDTH, RESOURCE.MARGIN], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    marginLeft: 240,
-    width: `calc(100% - 240px)`,
-    transition: theme.transitions.create(["width", "margin"], {
+    marginLeft: RESOURCE.NUMBER.TWO_HUNDRED_FORTY,
+    width: RESOURCE.NAVBAR_WIDTH,
+    transition: theme.transitions.create([RESOURCE.WIDTH, RESOURCE.MARGIN], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
