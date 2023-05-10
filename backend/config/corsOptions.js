2ffb6a5ba67146bf0ab5880478739a58cf2d1d2e
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:12:18 +0800

    Refactor corsOptions

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,8 +1,9 @@
 const allowedOrigins = require("./allowedOrigins");
+const { STATUSCODE } = require("../constants/index");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== STATUSCODE.NEGATIVE_ONE || !origin) {
       callback(null, true);
     } else callback(new Error("Not allowed by CORS"));
   },
