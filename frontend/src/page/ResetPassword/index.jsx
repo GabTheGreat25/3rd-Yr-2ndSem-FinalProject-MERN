8e0589747e88d753c87c0527e61d1f0aab84082f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:23:40 +0800

    Refactor ResetPassword

--- frontend/src/page/ResetPassword/index.jsx
+++ frontend/src/page/ResetPassword/index.jsx
@@ -7,20 +7,20 @@ import {
   InputAdornment,
   IconButton,
 } from "@mui/material";
-import { useResetPasswordMutation } from "../../state/api/reducer";
+import { useResetPasswordMutation } from "@api";
 import { Box } from "@mui/system";
 import { useFormik } from "formik";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { resetPasswordValidation } from "../../validation";
+import { resetPasswordValidation } from "@/validation";
 import { PacmanLoader } from "react-spinners";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { TAGS } from "../../constants";
+import { TAGS, RESOURCE, ERROR } from "@/constants";
 
 export default function () {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [resetPassword, { isLoading }] = useResetPasswordMutation();
+  const [resetPassword, { isLoading, isError }] = useResetPasswordMutation();
 
   const formik = useFormik({
     initialValues: {
@@ -38,38 +38,40 @@ export default function () {
       }).then((response) => {
         const toastProps = {
           position: toast.POSITION.TOP_RIGHT,
-          autoClose: 5000,
+          autoClose: RESOURCE.NUMBER.FIVE_THOUSAND,
         };
         if (response?.data?.success === true) {
           window.open(`https://mailtrap.io/inboxes`, "_blank");
           toast.success(`${response?.data?.message}`, toastProps);
-        } else {
+        } else
           toast.error(`${response?.error?.data?.error?.message}`, toastProps);
-        }
       });
     },
   });
 
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
 
-  const handleClickShowConfirmPassword = () => {
+  const handleClickShowConfirmPassword = () =>
     setShowConfirmPassword(!showConfirmPassword);
-  };
 
   return (
     <>
       {isLoading ? (
         <div className="loader">
-          <PacmanLoader color="#2c3e50" loading={true} size={50} />
+          <PacmanLoader
+            color="#2c3e50"
+            loading={true}
+            size={RESOURCE.NUMBER.FIFTY}
+          />
         </div>
+      ) : isError ? (
+        <div className="errorMessage">{ERROR.RESET_PASSWORD_ERROR}</div>
       ) : (
         <>
           <Container maxWidth="sm">
             <Box
               sx={{
-                mt: 4,
+                mt: RESOURCE.NUMBER.FOUR,
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
@@ -143,7 +145,7 @@ export default function () {
                 <Button
                   type="submit"
                   variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
+                  sx={{ mt: RESOURCE.NUMBER.THREE, mb: RESOURCE.NUMBER.TWO }}
                   disabled={!formik.isValid}
                 >
                   Reset Password
