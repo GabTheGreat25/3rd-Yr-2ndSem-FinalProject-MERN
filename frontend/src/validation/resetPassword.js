ebc19676262f6a4aa69dbd53c47a9480a27eca78
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:28:02 +0800

    Refactor resetPassword validation

--- frontend/src/validation/resetPassword.js
+++ frontend/src/validation/resetPassword.js
@@ -1,9 +1,10 @@
 import * as yup from "yup";
+import { RESOURCE } from "@/constants";
 
 export default yup.object({
   newPassword: yup
     .string("Enter your new password")
-    .min(8, "Password must be at least 8 characters long")
+    .min(RESOURCE.NUMBER.EIGHT, "Password must be at least 8 characters long")
     .required("Password is required"),
   confirmPassword: yup
     .string("Enter your confirm password")
