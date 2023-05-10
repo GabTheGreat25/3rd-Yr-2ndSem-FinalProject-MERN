998708743f23e906173403312c5c4e26a6a491da
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 12:21:16 +0800

    Add allowed origin in deployment

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "http://localhost:6969",
+  "https://3rd-yr-2nd-sem-final-project-mern-backend.vercel.app/",
   "https://www.camrentals.com",
   "https://camrentals.com",
 ];
