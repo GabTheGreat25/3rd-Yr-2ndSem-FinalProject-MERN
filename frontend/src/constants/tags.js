3a7cc2baa1cd80ccda399a1000d3ad31c05a9f60
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:18:42 +0800

    Refactor tags constant

--- frontend/src/constants/tags.js
+++ frontend/src/constants/tags.js
@@ -2,9 +2,12 @@ export default {
   API: "api",
   AUTH: "auth",
   USERS: "Users",
+  USER: "user",
   NOTES: "Notes",
   CAMERAS: "Cameras",
   TRANSACTIONS: "Transactions",
+  TRANSACTION: "transaction",
   COMMENTS: "Comments",
   EMAIL: "email",
+  CAMERA: "cameras",
 };
