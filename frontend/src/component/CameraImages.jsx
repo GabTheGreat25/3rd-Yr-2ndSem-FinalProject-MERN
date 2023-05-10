682a1ba0af14aa01f9542dcee094d0d5afac3baa
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-05-10 11:16:06 +0800

    Refactor CameraImages

--- frontend/src/component/CameraImages.jsx
+++ frontend/src/component/CameraImages.jsx
@@ -1,23 +1,26 @@
-import React from "react";
 import { Box, CardMedia } from "@mui/material";
+import { RESOURCE } from "@/constants";
 
 export default function (props) {
   const { image } = props;
   const numImages = image.length;
-  let height = "100px";
-  let width = "100px";
+  let height = "6.25rem";
+  let width = "6.25rem";
   let margin = "0";
-  let flexBasis = "100px";
+  let flexBasis = "6.25rem";
 
-  if (numImages > 1 && numImages <= 4) {
-    margin = "5px";
-    flexBasis = "calc(25% - 10px)";
-  } else if (numImages > 4 && numImages <= 9) {
-    margin = "5px";
-    flexBasis = "calc(11.1% - 10px)";
-  } else if (numImages > 9) {
-    margin = "5px";
-    flexBasis = "calc(8.33% - 10px)";
+  if (numImages > RESOURCE.NUMBER.ONE && numImages <= RESOURCE.NUMBER.FOUR) {
+    margin = ".2rem";
+    flexBasis = "calc(25% - .5rem)";
+  } else if (
+    numImages > RESOURCE.NUMBER.FOUR &&
+    numImages <= RESOURCE.NUMBER.NINE
+  ) {
+    margin = ".2rem";
+    flexBasis = "calc(11.1% - .5rem)";
+  } else if (numImages > RESOURCE.NUMBER.NINE) {
+    margin = ".2rem";
+    flexBasis = "calc(8.33% - .5rem)";
   }
 
   return (
@@ -26,7 +29,7 @@ export default function (props) {
         sx={{
           height: "100%",
           width: "100%",
-          borderRadius: 2,
+          borderRadius: RESOURCE.NUMBER.TWO,
           display: "flex",
           flexDirection: "row",
           flexWrap: "wrap",
@@ -39,12 +42,12 @@ export default function (props) {
             sx={{
               height: height,
               width: width,
-              borderRadius: 2,
+              borderRadius: RESOURCE.NUMBER.TWO,
               margin: margin,
               flexBasis: flexBasis,
             }}
-            image={imageItem.url}
-            alt={imageItem.alt}
+            image={imageItem?.url}
+            alt={imageItem?.alt}
           />
         ))}
       </Box>
